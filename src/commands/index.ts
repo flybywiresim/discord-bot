54e12da3eec6e437c3ba5d5a1a497a04f5d961c5
benjozork <benjamin.dupont.984@gmail.com>
2021-07-05 18:56:58 -0400

    fix: aliases
    (cherry picked from commit 9224f0d65905a64e39237e2d92a51f0e668f8c03)

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -14,8 +14,10 @@ import { versions } from './versions';
 import { membercount } from './membercount';
 import { installer } from './installer';
 import { roleinfo } from './roleinfo';
+import { CommandDefinition } from '../lib/command';
+import Logger from '../lib/logger';
 
-export default {
+const commands: CommandDefinition[] = [
     ping,
     help,
     bruheg,
@@ -32,4 +34,17 @@ export default {
     membercount,
     installer,
     roleinfo,
-};
+];
+
+const commandsObject: { [k: string]: CommandDefinition } = {};
+
+for (const def of commands) {
+    for (const name of (typeof def.name === 'string' ? [def.name] : def.name)) {
+        if (commandsObject[name]) {
+            Logger.warn(`Duplicate command/alias inserted: ${name}`);
+        }
+        commandsObject[name] = def;
+    }
+}
+
+export default commandsObject;
