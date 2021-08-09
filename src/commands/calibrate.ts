62f15d11f1475096703cf2342fce300403fd2881
Ben Williams <bwilliams12@me.com>
2021-08-09 21:16:24 +0100

    Update calibrate.ts and utf-8.ts to use makeLines
    Update calibrate.ts to include new lonk to docs site

--- src/commands/calibrate.ts
+++ src/commands/calibrate.ts
@@ -1,6 +1,6 @@
 import { CommandDefinition } from '../lib/command';
 import { CommandCategory } from '../constants';
-import { makeEmbed } from '../lib/embed';
+import { makeEmbed, makeLines } from '../lib/embed';
 
 export const calibrate: CommandDefinition = {
     name: 'calibrate',
@@ -8,11 +8,15 @@ export const calibrate: CommandDefinition = {
     category: CommandCategory.FBW,
     executor: (msg) => msg.channel.send(makeEmbed({
         title: 'FlyByWire A32NX | Throttle Calibration',
-        description: 'You need to calibrate your throttles\n'
-                + '\n'
-                + '1. Make sure you have a linear throttle axis. This can be achieved by setting Sensitivity + and -, centre and deadzone to 0.\n'
-                + '2. If you are using your throttles as reversers (without toggling with a button), make sure to enable reverse on axis, otherwise, leave this off.\n'
-                + '3. Watch [this video](https://www.youtube.com/watch?v=8yZuv2L4jPA&ab_channel=FlyByWireSimulations) in full, then calibrate your throttles.\n'
-                + '4. If you still have issues you may need to make your detent ranges larger. This can be done on the calibration page and making the range value higher. 0.05 is a good starting point.',
+        description: makeLines ([
+                'You need to calibrate your throttles',
+                ,
+                '1. Make sure you have a linear throttle axis. This can be achieved by setting Sensitivity + and -, centre and deadzone to 0.',
+                '2. If you are using your throttles as reversers (without toggling with a button), make sure to enable reverse on axis, otherwise, leave this off.',
+                '3. Watch [this video](https://www.youtube.com/watch?v=8yZuv2L4jPA&ab_channel=FlyByWireSimulations) in full, then calibrate your throttles.',
+                '4. If you still have issues you may need to make your detent ranges larger. This can be done on the calibration page and making the range value higher. 0.05 is a good starting point.',
+                ,
+                'Please visit [this link](https://docs.flybywiresim.com/start/throttle-calibration/) for a detailed tutorial with examples!',
+        ]),
     })),
 };
--- src/commands/utf-8.ts
+++ src/commands/utf-8.ts
@@ -1,6 +1,6 @@
 import { CommandDefinition } from '../lib/command';
 import { CommandCategory } from '../constants';
-import { makeEmbed } from '../lib/embed';
+import { makeEmbed, makeLines } from '../lib/embed';
 
 export const utf8: CommandDefinition = {
     name: 'utf',
@@ -8,14 +8,15 @@ export const utf8: CommandDefinition = {
     category: CommandCategory.FBW,
     executor: (msg) => msg.channel.send(makeEmbed({
         title: 'FlyByWire A32NX | UTF-8',
-        description: 'In rare cases the Autopilot, FADEC and electrical systems may not start or behave erratically. This is in part due to UTF-8 language support beta not enabled on your machine.\n'
-                + '\n'
-                + 'Solution:\n'
-                + '\n'
-                + '1. Open Windows Control Panel -> Region\n'
-                + '2. Go to the Administrative tab and click Change system locale\n'
-                + '3. Make sure the check mark next to Beta: Use UTF-8 for worldwide language support is selected\n'
-                + '4. Click OK and restart your computer',
+        description: makeLines(['In rare cases the Autopilot, FADEC and electrical systems may not start or behave erratically. This is in part due to UTF-8 language support beta not enabled on your machine.',
+                ,
+                'Solution:',
+                ,
+                '1. Open Windows Control Panel -> Region',
+                '2. Go to the Administrative tab and click Change system locale',
+                '3. Make sure the check mark next to Beta: Use UTF-8 for worldwide language support is selected',
+                '4. Click OK and restart your computer',
+        ]),
     })),
 };
 
