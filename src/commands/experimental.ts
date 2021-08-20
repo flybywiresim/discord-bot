da65041f79d11f62233f93217c256c3339ff0890
NUT <87286435+NUTTTTTTTT@users.noreply.github.com>
2021-08-20 20:07:49 +0100

    Update experimental.ts for exp
    Updates .exp as experimental back in use

--- src/commands/experimental.ts
+++ src/commands/experimental.ts
@@ -8,11 +8,12 @@ export const experimental: CommandDefinition = {
     category: CommandCategory.FBW,
     executor: (msg) => msg.channel.send(makeEmbed({
         title: 'FlyByWire A32NX | Experimental Version',
-        description: 'The experimental branch is currently on hold until further notice. '
-                + 'We will notify you through our #server-announcements when we have integrated the custom FPM and LNAV and make the experimental version available again. '
-                + 'Until then please use the development version.',
+        description: 'The experimental branch is now back in use! '
+                + 'This version is similar to the development version, but contains custom systems in progress, including the new FBW Custom Flight Management System (cFMS). '
+                + 'Experimental version will be updated with the latest changes to the "custom-fpm" branch and development version regularly. '
+                + '\n> No support will be offered via Discord for this version. ',
         fields: [
-            { name: 'Where can I download the Development version? ', value: '[Via the installer here](https://api.flybywiresim.com/installer)', inline: true },
+            { name: 'Where can I download the Experimental version? ', value: '[Via the installer here](https://api.flybywiresim.com/installer)', inline: true },
         ],
     })),
 };
