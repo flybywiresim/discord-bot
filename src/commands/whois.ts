477febe1d00e3d6ad693b6603dd164d3be0b675b
Jack <jackli0925@icloud.com>
2021-08-26 19:01:24 +0800

    ability to mention a user for information

--- src/commands/whois.ts
+++ src/commands/whois.ts
@@ -14,44 +14,49 @@ export const whois: CommandDefinition = {
     name: 'whois',
     description: 'Provides an embedded message with information about the mentioned user',
     category: CommandCategory.PUBLIC,
-    executor: (msg) => msg.channel.send(makeEmbed({
-        author: {
-            name: msg.author.username,
-            icon_url: msg.author.avatarURL(),
-        },
-        description: `<@!${msg.author.id}>`,
-        thumbnail: {
-            url: msg.author.avatarURL()
-        },
-        fields: [
-            {
-                name: "Username",
-                value: msg.author.tag,
-                inline: true
+    executor: async (msg) => {
+        let query = msg.mentions.members.toJSON()[0]?.userID;
+        let targetMember = query ? await msg.guild.members.fetch(query) : msg.member;
+
+        return msg.channel.send(makeEmbed({
+            author: {
+                name: targetMember.user.username,
+                icon_url: targetMember.user.avatarURL(),
             },
-            {
-                name: "Status",
-                value: beautifiedStatus[msg.author.presence.status.toUpperCase()],
-                inline: true
+            description: `<@!${targetMember.id}>`,
+            thumbnail: {
+                url: targetMember.user.avatarURL()
             },
-            {
-                name: "Joined",
-                value: moment(msg.member.joinedTimestamp).format("llll"),
-                inline: true
-            },
-            {
-                name: "Registered",
-                value: moment(msg.author.createdTimestamp).format("llll"),
-                inline: false
-            },
-            {
-                name: "Roles",
-                value: `<@&${msg.member.roles.cache.sort((a, b) => b.rawPosition - a.rawPosition).keyArray().filter(x => x != msg.guild.id).join("> <@&")}>`
-            },
-            {
-                name: "Permissions",
-                value: msg.member.permissions.toArray().join(", ").toLowerCase().replace(/_/g, " ").replace(/(^\w{1})|(\s+\w{1})/g, char => char.toUpperCase())
-            }
-        ]
-    }))
+            fields: [
+                {
+                    name: "Username",
+                    value: targetMember.user.tag,
+                    inline: true
+                },
+                {
+                    name: "Status",
+                    value: beautifiedStatus[targetMember.presence.status.toUpperCase()],
+                    inline: true
+                },
+                {
+                    name: "Joined",
+                    value: moment(targetMember.joinedTimestamp).format("llll"),
+                    inline: true
+                },
+                {
+                    name: "Registered",
+                    value: moment(targetMember.user.createdTimestamp).format("llll"),
+                    inline: false
+                },
+                {
+                    name: "Roles",
+                    value: `<@&${targetMember.roles.cache.sort((a, b) => b.rawPosition - a.rawPosition).keyArray().filter(x => x != msg.guild.id).join("> <@&")}>`
+                },
+                {
+                    name: "Permissions",
+                    value: targetMember.permissions.toArray().join(", ").toLowerCase().replace(/_/g, " ").replace(/(^\w{1})|(\s+\w{1})/g, char => char.toUpperCase())
+                }
+            ]
+        }))
+    }
 };
