7232c931286f83269f34fdbe044b9db1b49ecb0e
NUT <87286435+NUTTTTTTTT@users.noreply.github.com>
2021-09-05 22:57:35 +0100

    Fix: cockpit interaction reformat (#23)
    * Reformat .legacy
    
    * Reformat .controls
    
    * reformat .msfs and .cockpit

--- src/commands/controls.ts
+++ src/commands/controls.ts
@@ -7,32 +7,20 @@ export const controls: CommandDefinition = {
     description: 'Instructions on how to switch back to legacy controls and how to use new controls',
     category: CommandCategory.FBW,
     executor: (msg) => msg.channel.send(makeEmbed({
-        title: 'MSFS | Cockpit Interaction System',
+        title: 'MSFS | Cockpit Interaction System ',
         fields: [
             {
-                name: 'Legacy Cockpit Interaction System',
+                name: 'Lock or Legacy? ',
                 value: makeLines([
-                    'Go to menu',
-                    'General Options',
-                    'Accessibility',
-                    'Find the Cockpit Interaction System setting',
-                    'Change to legacy',
+                    'Sim Update 5 introduced a new cockpit interaction system, which changes the way you interact with different buttons and switches. '
+                  + 'You can find out more on the new interaction system [here](https://docs.flybywiresim.com/fbw-a32nx/support/reported-issues/#cockpit-interaction-system), or if you\'d prefer to switch back to the old way:',
+                    '',
+                    ' - Open the settings menu.',
+                    ' - Select \'General Options\' and then select \'Accessibility\'. ',
+                    ' - Find the \'Cockpit Interaction System\' option and set this to Legacy.',
                 ]),
                 inline: false,
             },
-            {
-                name: 'Using the new cockpit interaction system',
-                value: makeLines([
-                    'Highlight a control (like a knob)',
-                    'Hold left click to lock that control. Now your mouse will not affect other controls or other mouse bindings',
-                    'Move the mouse left to turn the knob left, move it right to turn the knob to the right (with left click held down)',
-                    'You can also use the scroll wheel while holding left click down to turn the knob to the left or right',
-                    'To push a control/knob in, lock to the control using left click and then right click',
-                    'To pull a control/knob out, hold left click and then click your scroll wheel (middle mouse)',
-                    'Note: If you already use middle mouse button to activate freelook this may not work. Check your keybinds so this feature does not conflict',
-                ]),
-                inline: false,
-            }, 
         ],
     })),
 };
