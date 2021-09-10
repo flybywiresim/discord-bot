361cc619fa7640ca157c29609ca57851ebfa7bb8
NUTTTTTTTT <Haldenbyc@gmail.com>
2021-09-10 19:34:31 +0100

    add .boris

---
+++ src/commands/boris.ts
@@ -0,0 +1,12 @@
+import { CommandDefinition } from '../lib/command';
+import { makeEmbed } from '../lib/embed';
+import { CommandCategory } from '../constants';
+
+const BORIS_URL = 'https://c.tenor.com/1bIsb5roaSsAAAAd/haggisbandit-sound.gif';
+
+export const boris: CommandDefinition = {
+    name: 'boris',
+    description: 'boris soudn',
+    category: CommandCategory.FBW,
+    executor: (msg) => msg.channel.send(makeEmbed({ image: { url: BORIS_URL } })),
+};
