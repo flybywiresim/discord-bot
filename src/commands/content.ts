da2219637bd0e4ce15c42531cd529117a5983e22
NUTTTTTTTT <Haldenbyc@gmail.com>
2021-09-10 20:19:52 +0100

    add further info to .content

--- src/commands/content.ts
+++ src/commands/content.ts
@@ -13,6 +13,8 @@ export const content: CommandDefinition = {
         description: makeLines([
             'In order to check which version of the A32NX you have installed, please open the MSFS Content Manager from the second page on the main menu and search for \'A32NX\'. '
           + 'Then take a screenshot showing the results and send it here in [#Support](https://discord.gg/U2Askt6r) as shown below. ',
+            '',
+            'If you find old versions or multiple installations, please delete them via the Content Manager and/or your Community folder.',
         ]),
         image: { url: CONTENTMANAGER_HELP_URL },
     })),
