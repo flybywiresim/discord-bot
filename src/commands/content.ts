648cbf42bb451ce59611340f3d722e443bc98739
NUT <87286435+NUTTTTTTTT@users.noreply.github.com>
2021-09-13 14:54:36 +0100

    replace Support link on .content
    Intentionally not using a vanity link as the link directs straight to support where people should already be. The invite link on previously however was incorrect as was meant to be replaced with one not expiring, which I've now done.

--- src/commands/content.ts
+++ src/commands/content.ts
@@ -12,7 +12,7 @@ export const content: CommandDefinition = {
         title: 'FlyByWire Support | Checking your aircraft version',
         description: makeLines([
             'In order to check which version of the A32NX you have installed, please open the MSFS Content Manager from the second page on the main menu and search for \'flybywire\'. '
-          + 'Then take a screenshot showing the results and send it here in [#Support](https://discord.gg/U2Askt6r) as shown below. ',
+          + 'Then take a screenshot showing the results and send it here in [#Support](https://discord.gg/tJebFgXrUX) as shown below. ',
             '',
             'If you find old versions or multiple installations, please delete them via the Content Manager and/or your Community folder.',
         ]),
