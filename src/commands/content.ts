2d481807dd294a28b57e7f1ee4c4e4dde1cb6685
NUT <87286435+NUTTTTTTTT@users.noreply.github.com>
2021-09-14 13:46:27 +0100

    fix: update .content command (#30)
    * update image on .content
    
    * add further info to .content
    
    * replace 'A32NX' with 'flybywire'
    
    * replace Support link on .content
    
    Intentionally not using a vanity link as the link directs straight to support where people should already be. The invite link on previously however was incorrect as was meant to be replaced with one not expiring, which I've now done.
    
    * fix content.ts command link

--- src/commands/content.ts
+++ src/commands/content.ts
@@ -2,7 +2,7 @@ import { CommandDefinition } from '../lib/command';
 import { makeEmbed, makeLines } from '../lib/embed';
 import { CommandCategory } from '../constants';
 
-const CONTENTMANAGER_HELP_URL = 'https://docs.flybywiresim.com/fbw-a32nx/assets/support-guide/Content-Manager.jpg';
+const CONTENTMANAGER_HELP_URL = 'https://media.discordapp.net/attachments/740722295009706034/885966763089625088/unknown.png';
 
 export const content: CommandDefinition = {
     name: ['content', 'contentmanager'],
@@ -11,8 +11,10 @@ export const content: CommandDefinition = {
     executor: (msg) => msg.channel.send(makeEmbed({
         title: 'FlyByWire Support | Checking your aircraft version',
         description: makeLines([
-            'In order to check which version of the A32NX you have installed, please open the MSFS Content Manager from the second page on the main menu and search for \'A32NX\'. '
-          + 'Then take a screenshot showing the results and send it here in [#Support](https://discord.gg/U2Askt6r) as shown below. ',
+            'In order to check which version of the A32NX you have installed, please open the MSFS Content Manager from the second page on the main menu and search for \'flybywire\'. '
+          + 'Then take a screenshot showing the results and send it here in <#785976111875751956> as shown below. ',
+            '',
+            'If you find old versions or multiple installations, please delete them via the Content Manager and/or your Community folder.',
         ]),
         image: { url: CONTENTMANAGER_HELP_URL },
     })),
