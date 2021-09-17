cfae0539aed583dc2b28d3dc43ba18b548d268bc
NUTTTTTTTT <Haldenbyc@gmail.com>
2021-09-17 16:16:18 +0100

    changed Simbrief to SimBrief

--- src/commands/airframe.ts
+++ src/commands/airframe.ts
@@ -7,7 +7,7 @@ export const airframe: CommandDefinition = {
     description: 'Provides a link to the updated Simbrief airframe',
     category: CommandCategory.FBW,
     executor: (msg) => msg.channel.send(makeEmbed({
-        title: 'FlyByWire A32NX | Simbrief Airframe',
-        description: 'Our updated Simbrief airframe for the A32NX with correct weights is available [here](https://www.simbrief.com/system/dispatch.php?sharefleet=337364_1631550522735). This is a new airframe based on our updated flight model, and will always be kept up-to-date.'
+        title: 'FlyByWire A32NX | SimBrief Airframe',
+        description: 'Our updated SimBrief airframe for the A32NX with correct weights is available [here](https://www.simbrief.com/system/dispatch.php?sharefleet=337364_1631550522735). This is a new airframe based on our updated flight model, and will always be kept up-to-date.'
     })),
 };
