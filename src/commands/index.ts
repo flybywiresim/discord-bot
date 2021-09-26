a1fdf9a64c3ed2d31a9f8334ba1793864b86c9eb
NUT <87286435+NUTTTTTTTT@users.noreply.github.com>
2021-09-26 11:56:13 +0100

    add .willithave, update index.ts (#45)

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -34,6 +34,7 @@ import { boris } from './boris';
 import { afloor } from './afloor';
 import { airframe } from './airframe';
 import { xbox } from './xbox';
+import { willithave } from './willithave';
 import { CommandDefinition } from '../lib/command';
 import Logger from '../lib/logger';
 
@@ -74,6 +75,7 @@ const commands: CommandDefinition[] = [
     afloor,
     airframe,
     xbox,
+    willithave,
 ];
 
 const commandsObject: { [k: string]: CommandDefinition } = {};
