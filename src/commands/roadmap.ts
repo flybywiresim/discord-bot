4816b816a54649fcb8d8b92be4e00dabcc99f597
NUT <87286435+NUTTTTTTTT@users.noreply.github.com>
2021-10-02 13:02:25 +0100

    Update src/commands/roadmap.ts
    Co-authored-by: BenW <bwilliams12@me.com>

--- src/commands/roadmap.ts
+++ src/commands/roadmap.ts
@@ -5,7 +5,7 @@ import { CommandCategory } from '../constants';
 const ROADMAP_URL = 'https://cdn.discordapp.com/attachments/740722295009706034/893674991667798026/ROADMAP.png';
 
 export const roadmap: CommandDefinition = {
-    name: 'roadmap',
+    name: ['roadmap', 'goals'],
     category: CommandCategory.FBW,
     description: 'FBW Roadmap',
     executor: (msg) => msg.channel.send(makeEmbed({ image: { url: ROADMAP_URL } })),
