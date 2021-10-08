09ada51f4574dde50c15b1fc1bfbe40a47c9f995
BenW <bwilliams12@me.com>
2021-10-08 17:15:17 +0100

    fix: update .screens and .efb (#54)
    * Update .screens image
    
    * Update wording
    
    * Update .efb gif

--- src/commands/efb.ts
+++ src/commands/efb.ts
@@ -2,7 +2,7 @@ import { CommandDefinition } from '../lib/command';
 import { makeEmbed } from '../lib/embed';
 import { CommandCategory } from '../constants';
 
-const EFB_URL = 'https://cdn.discordapp.com/attachments/741669372195504200/806287507855638618/ezgif.com-video-to-gif-4.gif';
+const EFB_URL = 'https://cdn.discordapp.com/attachments/885885609007276062/895429612119683082/EFB2.gif';
 
 export const efb: CommandDefinition = {
     name: 'efb',
