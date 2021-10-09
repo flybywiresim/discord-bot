fae530117b5060f340dd564362964dac3d05575c
Ben Williams <bwilliams12@me.com>
2021-10-09 13:56:37 +0100

    Add content

--- src/commands/clean-install.ts
+++ src/commands/clean-install.ts
@@ -3,13 +3,13 @@ import { makeEmbed, makeLines } from '../lib/embed';
 import { CommandCategory } from '../constants';
 
 export const clean: CommandDefinition = {
-    name: ['clean', 'clean install', 'order66'],
+    name: ['clean', 'clean install', 'cleaninstall', 'order66'],
     description: 'Clean Install',
     category: CommandCategory.FBW,
     executor: (msg) => msg.channel.send(makeEmbed({
         title: 'FlyByWire A32NX | Clean Install',
         description: makeLines([
-            'Content Here',
+            'We suggest trying a clean install. Please see [this guide](https://docs.flybywiresim.com/fbw-a32nx/installation/?h=clean+install#clean-install-steps) for detailed instructions.',
         ]),
     })),
 };
