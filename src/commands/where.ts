283148eae3bbfe686cf85584913736d6a013f478
oim1 <oim.oom.imo@gmail.com>
2021-10-14 22:37:57 +1100

    Update src/commands/where.ts
    Co-authored-by: BenW <bwilliams12@me.com>

--- src/commands/where.ts
+++ src/commands/where.ts
@@ -9,7 +9,7 @@ export const where: CommandDefinition = {
     description: 'Provides an image of which aircraft to select in the aircraft selector',
     category: CommandCategory.FBW,
     executor: (msg) => msg.channel.send(makeEmbed({
-        title: 'FlyByWire Support | Where is my aircraft?',
+        title: 'FlyByWire A32NX | Where is my aircraft?',
         description: 'The FlyByWire A32NX addon is now separate from the default a320neo. Make sure you are selecting FlyByWire Simulations A320neo (LEAP) in the aircraft selector instead of the default a320neo.',
         image: { url: WHERE_IMAGE_URL },
         footer: { text: 'Tip: Click the image to view in full size' }
