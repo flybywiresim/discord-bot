a702ee1f63fecf2adbf4ac2f27f4012a7507d033
oim1 <81839029+oim1@users.noreply.github.com>
2021-10-16 11:37:07 +1100

    added periods to the end of sentences

--- src/commands/printer.ts
+++ src/commands/printer.ts
@@ -8,8 +8,8 @@ export const printer: CommandDefinition = {
     category: CommandCategory.FBW,
     executor: (msg) => msg.channel.send(makeEmbed({
         title: 'FlyByWire A32NX | How do I use the printer?',
-        description: 'Please watch our video tutorial on how to use the FlyByWire A32NX printer [here](https://www.youtube.com/watch?v=i3ughyFtnCM)',
-        footer: { text: 'Warning: Using the printer may cause a crash to desktop. This issue occurs randomly and we are currently looking for ways to reproduce and resolve it'}
+        description: 'Please watch our video tutorial on how to use the FlyByWire A32NX printer [here.](https://www.youtube.com/watch?v=i3ughyFtnCM)',
+        footer: { text: 'Warning: Using the printer may cause a crash to desktop. This issue occurs randomly and we are currently looking for ways to reproduce and resolve it.'}
     })),
 };
 
