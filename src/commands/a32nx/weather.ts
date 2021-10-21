b441632c508adbce96c609f2a37a7b3a62c643a2
oim1 <81839029+oim1@users.noreply.github.com>
2021-10-22 10:28:54 +1100

    updated weather.ts

--- src/commands/a32nx/weather.ts
+++ src/commands/a32nx/weather.ts
@@ -1,6 +1,6 @@
 import { CommandDefinition } from '../../lib/command';
 import { CommandCategory } from '../../constants';
-import { makeEmbed } from '../../lib/embed';
+import { makeEmbed, makeLines } from '../../lib/embed';
 
 export const weather: CommandDefinition = {
     name: ['weather', 'wx'],
@@ -8,6 +8,10 @@ export const weather: CommandDefinition = {
     category: CommandCategory.FBW,
     executor: (msg) => msg.channel.send(makeEmbed({
         title: 'FlyByWire A32NX | Weather + Terrain Radars',
-        description: 'The experimental version of the A32NX does not have operating weather and terrain radars. This is due to the lack of realism in the default radars. The team is currently waiting for Asobo to implement weather and terrain API\'s in order to create a true to life radar system. You can read the MSFS forum thread [here.](https://forums.flightsimulator.com/t/implement-weather-and-terrain-api-s-for-aircraft-developers-to-implement-accurate-radar-predictive-windshear-egpws-and-metar-wind-uplink/442016)',
+        description: makeLines(['The experimental version of the A32NX does not have operating weather and terrain radars. This is due to the performance issues related to the default weather and terrain radars and the ND being rewritten.',
+        ,
+        'The team is also currently waiting on a weather and terrain API to be implemented in order to make a radar that is as realistic as possible. You can read the MSFS forum [here.](https://forums.flightsimulator.com/t/implement-weather-and-terrain-api-s-for-aircraft-developers-to-implement-accurate-radar-predictive-windshear-egpws-and-metar-wind-uplink/442016)'
+        ]),
     })),
 };
+
