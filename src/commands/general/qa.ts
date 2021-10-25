24707c3857434bb467b005b00cf6856a9c1a7497
NUTTTTTTTT <Haldenbyc@gmail.com>
2021-10-25 16:05:20 +0100

    add qa command, update index.ts

---
+++ src/commands/general/qa.ts
@@ -0,0 +1,13 @@
+import { CommandDefinition } from '../../lib/command';
+import { CommandCategory } from '../../constants';
+import { makeEmbed } from '../../lib/embed';
+
+export const qa: CommandDefinition = {
+    name: 'qa',
+    description: 'Links to the Quality Assurance docs page',
+    category: CommandCategory.FBW,
+    executor: (msg) => msg.channel.send(makeEmbed({
+        title: 'FlyByWire | Quality Assurance',
+        description: 'Any additions/changes to the code in our aircraft are tested thoroughly by our Quality Assurance team. The team are built of members of our community, if you\'re interested in finding out more information on our QA process, or joining the team, please read our guide [here!](https://docs.flybywiresim.com/dev-corner/qa-process/)',
+    })),
+};
