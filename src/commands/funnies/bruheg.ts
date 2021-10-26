ea5a68bd071aeb700123dff99a79d989b7047ff0
BenW <bwilliams12@me.com>
2021-10-26 07:36:29 +0100

    spooky bruheg

--- src/commands/funnies/bruheg.ts
+++ src/commands/funnies/bruheg.ts
@@ -2,7 +2,7 @@ import { CommandDefinition } from '../../lib/command';
 import { makeEmbed } from '../../lib/embed';
 import { CommandCategory } from '../../constants';
 
-const BRUHEG_URL = 'https://media.discordapp.net/attachments/772990923486527534/790331925398945852/xbGbh5H8sxGwAAAABJRU5ErkJggg.png';
+const BRUHEG_URL = 'https://cdn.discordapp.com/attachments/874018873433808946/902283096752291920/unknown.png';
 
 export const bruheg: CommandDefinition = {
     name: 'bruheg',
