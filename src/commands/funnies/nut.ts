4e6ab5ce0ff62d907cd9f14cb4dd105338b50561
BenW <bwilliams12@me.com>
2021-11-07 20:06:41 +0000

    original nut

--- src/commands/funnies/nut.ts
+++ src/commands/funnies/nut.ts
@@ -2,7 +2,7 @@ import { CommandDefinition } from '../../lib/command';
 import { makeEmbed } from '../../lib/embed';
 import { CommandCategory } from '../../constants';
 
-const NUT_URL = 'https://cdn.discordapp.com/attachments/874018873433808946/900139765154873414/meme.png';
+const NUT_URL = 'https://media.discordapp.net/attachments/838062729398976522/879523609926832198/meme.png';
 
 export const nut: CommandDefinition = {
     name: 'nut',
