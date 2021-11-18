affb6c76022de070a146236e42b62c39d698b580
Alex Cutforth <alex@densem.net>
2021-11-19 12:46:40 +1300

    fix: delete command on .synaptic and add .syn shorthand

--- src/commands/general/synaptic.ts
+++ src/commands/general/synaptic.ts
@@ -2,8 +2,11 @@ import { CommandDefinition } from '../../lib/command';
 import { CommandCategory } from '../../constants';
 
 export const synaptic: CommandDefinition = {
-    name: 'synaptic',
+    name: ['synaptic', 'syn'],
     description: 'Provides link to synaptic discord server',
     category: CommandCategory.GENERAL,
-    executor: (msg) => msg.channel.send('https://discord.gg/acQkSvrePG'),
+    executor: async (msg) => {
+        await msg.channel.send('https://discord.gg/acQkSvrePG');
+        await msg.delete();
+    },
 };
