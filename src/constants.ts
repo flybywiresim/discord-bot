723c8113ef282c1896ad0e2a0cd53213f554f263
Benjamin Dupont <benjozorkfr@gmail.com>
2021-11-20 16:26:11 -0500

    feat: add required role support to framework, extract message processing

--- src/constants.ts
+++ src/constants.ts
@@ -11,11 +11,16 @@ export enum CommandCategory {
     MODERATION = 'Moderation',
 }
 
+export enum Guilds {
+    FBW = '738864299392630914',
+    MSFS_DN = '869577697616068638',
+}
+
 export enum Channels {
     MOD_LOGS = '783996780181585921',
     USER_LOGS = '779944761699729418',
 }
 
 export const UserLogExclude = [
-    '628400349979344919' // StickyBot
-]
+    '628400349979344919', // StickyBot
+];
--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,9 +1,11 @@
 import { userBanned } from './ban';
 import { messageDeleted } from './messagedelete';
 import { messageUpdated } from './messageupdate';
+import { message } from './message';
 
 export default [
     userBanned,
     messageDeleted,
-    messageUpdated
+    messageUpdated,
+    message,
 ];
