fd8e3b59ed25be34296a3580523306eddab8487b
Ben Williams <bwilliams12@me.com>
2021-11-21 13:47:13 +0000

    Change category

--- src/commands/support/discontinuity.ts
+++ src/commands/support/discontinuity.ts
@@ -5,9 +5,9 @@ import { makeEmbed, makeLines } from '../../lib/embed';
 export const discontinuity: CommandDefinition = {
     name: ['discontinuity', 'disco'],
     description: 'Link to docs about discontinuities',
-    category: CommandCategory.SUPPORT,
+    category: CommandCategory.A32NX,
     executor: (msg) => msg.channel.send(makeEmbed({
-        title: 'FlyByWire Support | Discontinuity',
+        title: 'FlyByWire A32NX | Discontinuity',
         description: makeLines ([
             'Please see our [documentation](https://docs.flybywiresim.com/pilots-corner/beginner-guide/preparing-mcdu/#discontinuity) for information on what to do with a discontinuity in your flight plan.',
         ]),
