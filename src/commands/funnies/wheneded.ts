f2239c0fd6dcd4bd0a7e85728d9970c85000c199
Erick Torres <30361843+ErickSharp@users.noreply.github.com>
2021-11-23 21:34:24 -0600

    refactor: add wheneded to index (#106)
    * fix: add whended to index
    
    * refactor: change names

--- src/commands/funnies/wheneded.ts
+++ src/commands/funnies/wheneded.ts
@@ -3,9 +3,9 @@ import { CommandCategory } from '../../constants';
 
 const WHENDED_URL = 'https://cdn.discordapp.com/attachments/740722295009706034/912043650601586769/dot_when.mov';
 
-export const whended: CommandDefinition = {
-    name: 'whended',
-    description: 'A wild .when appears!',
+export const wheneded: CommandDefinition = {
+    name: 'wheneded',
+    description: 'A wild .whened appears!',
     category: CommandCategory.FUNNIES,
-    executor: (msg) => msg.channel.send( WHENDED_URL ),
+    executor: (msg) => msg.channel.send(WHENDED_URL),
 };
