dcea0c7cf932695cbef0cb7f8dd4826c67c9965b
Cleon <81078099+xZreai@users.noreply.github.com>
2021-11-24 23:09:36 +0400

    Update tug.ts

--- src/commands/support/tug.ts
+++ src/commands/support/tug.ts
@@ -9,9 +9,9 @@ export const tug: CommandDefinition = {
     executor: (msg) => msg.channel.send(makeEmbed({
         title: 'FlyByWire Support | My Aircraft is Stuck!',
         description: makeLines([
-           'Make sure you do not have the "NW STRG DISC" message on the upper ECAM display. ',
-           'If you do, please press "Shift" + "P" on your keyboard to disconnect the invisible pushback tug.',
-           'Please read our guide [here](https://docs.flybywiresim.com/fbw-a32nx/support/reported-issues/#nose-wheel-steering-locked-nw-strg-disc) for more information. ',
+            'Make sure you do not have the "NW STRG DISC" message on the upper ECAM display. ',
+            'If you do, please press "Shift" + "P" on your keyboard to disconnect the invisible pushback tug.',
+            'Please read our guide [here](https://docs.flybywiresim.com/fbw-a32nx/support/reported-issues/#nose-wheel-steering-locked-nw-strg-disc) for more information. ',
         ]),
     })),
 };
