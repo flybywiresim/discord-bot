5cd472b152730b472e0d9a99100cfff8a4f9b323
Ben Williams <bwilliams12@me.com>
2021-11-28 10:41:33 +0000

    Add support us

--- src/commands/moderation/welcome.ts
+++ src/commands/moderation/welcome.ts
@@ -4,7 +4,7 @@ import { CommandCategory } from '../../constants';
 
 const LANDING_IMAGE_URL = 'https://media.discordapp.net/attachments/864493190471352344/893458239411322910/Landing_Center_FB.png?width=1399&height=683';
 const SOCIAL_IMAGE_URL = 'https://cdn.discordapp.com/attachments/825674445342638120/909527268714496000/social_media.png';
-///const SUPPORT_IMAGE_URL = 'https://cdn.discordapp.com/attachments/825674445342638120/909527264218210334/support.png';
+const SUPPORT_IMAGE_URL = 'https://cdn.discordapp.com/attachments/825674445342638120/909527264218210334/support.png';
 const HELP_IMAGE_URL = 'https://cdn.discordapp.com/attachments/825674445342638120/909527270564167690/help_and_support.png';
 const IMPORTANT_INFO_IMAGE_URL = 'https://cdn.discordapp.com/attachments/825674445342638120/909527271398834226/impt_info.png';
 
@@ -21,6 +21,16 @@ const SOCIAL_EMBED = [
     })
 ]
 
+const SUPPORT_EMBED = [
+    makeEmbed({
+        title: '<:Partnered:799926764407226410> FlyByWireSimulations | Support Us',
+        description: makeLines([
+            'You are able to voluntarily support us financially to ensure we are able to cover the costs of servers and developmental resources.',
+            '',
+            'https://opencollective.com/flybywire'
+        ])
+    })
+]
 
 const IMPORTANT_INFO_EMBED = [
     makeEmbed({
@@ -73,6 +83,10 @@ export const welcome: CommandDefinition = {
         await msg.channel.send({ files: [ SOCIAL_IMAGE_URL ]});
 
         await msg.channel.send(SOCIAL_EMBED);
+
+        await msg.channel.send({ files: [ SUPPORT_IMAGE_URL ]});
+
+        await msg.channel.send(SUPPORT_EMBED);
 
         await msg.channel.send({ files: [ HELP_IMAGE_URL ]});
 
