42d050e9259deef3e16be1595a9ec45482217ffe
NUTTTTTTTT <Haldenbyc@gmail.com>
2021-11-30 11:55:54 +0000

    update .afloor and .fixinfo docs links

--- src/commands/a32nx/afloor.ts
+++ src/commands/a32nx/afloor.ts
@@ -8,6 +8,6 @@ export const afloor: CommandDefinition = {
     category: CommandCategory.A32NX,
     executor: (msg) => msg.channel.send(makeEmbed({
         title: 'FlyByWire A32NX | Alpha Floor',
-        description: 'Please see our [Alpha Floor Protection Guide](https://docs.flybywiresim.com/fbw-a32nx/feature-guides/afloor/) or [Video Tool-Tip](https://youtu.be/iXQb675J9mA) for information on Alpha Floor Protection and how to prevent/recover from it.',
+        description: 'Please see our [Alpha Floor Protection Guide](https://docs.flybywiresim.com/pilots-corner/advanced-guides/protections/afloor/) or [Video Tool-Tip](https://youtu.be/iXQb675J9mA) for information on Alpha Floor Protection and how to prevent/recover from it.',
     })),
 };
