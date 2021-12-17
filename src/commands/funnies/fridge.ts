33082394c1eed320767f64cd8aa1457b6e8e53e7
oim1 <oim.oom.imo@gmail.com>
2021-12-17 11:49:03 +1100

    Update src/commands/funnies/fridge.ts
    Co-authored-by: NUT <87286435+NUTTTTTTTT@users.noreply.github.com>

--- src/commands/funnies/fridge.ts
+++ src/commands/funnies/fridge.ts
@@ -2,7 +2,7 @@ import { CommandDefinition } from '../../lib/command';
 import { makeEmbed } from '../../lib/embed';
 import { CommandCategory } from '../../constants';
 
-const FRIDGE_URL = 'https://media.discordapp.net/attachments/898602626436964402/921199183430549524/samsung-family-hub-refrigerator-1_1024x1024.png?width=676&height=676';
+const FRIDGE_URL = 'https://media.discordapp.net/attachments/898602626436964402/921199183430549524/samsung-family-hub-refrigerator-1_1024x1024.png';
 
 export const fridge: CommandDefinition = {
     name: 'fridge',
