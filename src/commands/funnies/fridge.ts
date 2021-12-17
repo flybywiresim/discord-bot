eb2ea4cf441ee16fe5d4caf38554ff3d10c8dc12
oim1 <oim.oom.imo@gmail.com>
2021-12-17 12:06:37 +1100

    fixed fridge image (#130)

--- src/commands/funnies/fridge.ts
+++ src/commands/funnies/fridge.ts
@@ -2,7 +2,7 @@ import { CommandDefinition } from '../../lib/command';
 import { makeEmbed } from '../../lib/embed';
 import { CommandCategory } from '../../constants';
 
-const FRIDGE_URL = 'https://media.discordapp.net/attachments/898602626436964402/921199183430549524/samsung-family-hub-refrigerator-1_1024x1024.png';
+const FRIDGE_URL = 'https://media.discordapp.net/attachments/898602626436964402/921205269927706675/samsung-family-hub-refrigerator-1_1024x1024clean.png';
 
 export const fridge: CommandDefinition = {
     name: 'fridge',
