e4bf6c90917ad254d7b9a1d5c61d700c5b915589
NUTTTTTTTT <Haldenbyc@gmail.com>
2021-12-19 17:48:18 +0000

    fix: .ping moderator only perms

--- src/commands/utils/ping.ts
+++ src/commands/utils/ping.ts
@@ -5,6 +5,7 @@ export const ping: CommandDefinition = {
     name: 'ping',
     description: 'Send back a message',
     category: CommandCategory.UTILS,
+    requiredPermissions: ['BAN_MEMBERS'],
     executor: (msg) => {
         const text = msg.content.replace(/\.ping\s*/, '');
 
