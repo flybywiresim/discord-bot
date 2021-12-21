4f2ef34b2f081d1e36cfa8abd73932323f042931
NUT <87286435+NUTTTTTTTT@users.noreply.github.com>
2021-12-21 17:22:37 +0000

    Update src/commands/utils/ping.ts
    Co-authored-by: oim1 <oim.oom.imo@gmail.com>

--- src/commands/utils/ping.ts
+++ src/commands/utils/ping.ts
@@ -5,7 +5,7 @@ export const ping: CommandDefinition = {
     name: 'ping',
     description: 'Send back a message',
     category: CommandCategory.UTILS,
-    requiredPermissions: ['BAN_MEMBERS'],
+    requiredPermissions: ['MANAGE_WEBHOOKS'],
     executor: (msg) => {
         const text = msg.content.replace(/\.ping\s*/, '');
 
