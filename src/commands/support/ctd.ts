de69ac962274677de8f59965d16f226d86a3064e
oim1 <81839029+oim1@users.noreply.github.com>
2021-12-25 01:13:54 +1100

    changed wording

--- src/commands/support/ctd.ts
+++ src/commands/support/ctd.ts
@@ -1,5 +1,5 @@
 import { CommandDefinition } from '../../lib/command';
-import { makeEmbed, makeLines } from '../../lib/embed';
+import { makeEmbed } from '../../lib/embed';
 import { CommandCategory } from '../../constants';
 
 export const ctd: CommandDefinition = {
@@ -8,6 +8,6 @@ export const ctd: CommandDefinition = {
     category: CommandCategory.SUPPORT,
     executor: (msg) => msg.channel.send(makeEmbed({
         title: 'FlyByWire Support | Crash To Desktop',
-        description: 'Find a collection of tips around Crash To Desktops [here.](https://docs.flybywiresim.com/fbw-a32nx/support/reported-issues/#ctd-crash-to-desktop)',
+        description: 'Find a collection of tips to potentially help resolve crash to desktops [here.](https://docs.flybywiresim.com/fbw-a32nx/support/reported-issues/#ctd-crash-to-desktop)',
     })),
 };
