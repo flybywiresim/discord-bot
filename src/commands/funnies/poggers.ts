2bdc621a75c9d26c110c3748f476d1bf375e7ad0
oim1 <81839029+oim1@users.noreply.github.com>
2021-12-27 03:19:12 +1100

    added new poggers alias

--- src/commands/funnies/poggers.ts
+++ src/commands/funnies/poggers.ts
@@ -4,7 +4,7 @@ import { CommandCategory } from '../../constants';
 const POGGERS_URL = 'https://tenor.com/view/lizard-dancing-poggers-lizard-dance-poggers-gif-18527737';
 
 export const poggers: CommandDefinition = {
-    name: 'poggers',
+    name: ['poggers', 'pog'],
     description: 'POG',
     category: CommandCategory.FUNNIES,
     executor: (msg) => msg.channel.send( POGGERS_URL ),
