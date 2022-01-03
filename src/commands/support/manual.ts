251cb1e12e68ea709e617c6740454b15ee4ee11f
Cleon <cleonthegamer@gmail.com>
2022-01-04 00:37:36 +0400

    Update manual.ts

--- src/commands/support/manual.ts
+++ src/commands/support/manual.ts
@@ -2,7 +2,7 @@ import { CommandDefinition } from '../../lib/command';
 import { makeEmbed } from '../../lib/embed';
 import { CommandCategory } from '../../constants';
 
-const MANUAL_URL = 'https://cdn.discordapp.com/attachments/752801628347957248/914937996514570300/FBWanim2_1.gif';
+const MANUAL_URL = 'https://cdn.discordapp.com/attachments/785976111875751956/927657411764969612/unknown.png';
 
 export const manual: CommandDefinition = {
     name: 'manual',
