f86968bcc7a67457dec77860006c49bb38f5221f
Sam <93292288+Earthsam12@users.noreply.github.com>
2022-01-06 10:06:04 -0600

    feat: Add .takeoff command (#145)
    * add takeoff perf command
    
    * Update src/commands/a32nx/takeoffPerf.ts
    
    Co-authored-by: oim1 <oim.oom.imo@gmail.com>
    
    * Update src/commands/a32nx/takeoffPerf.ts
    
    Co-authored-by: oim1 <oim.oom.imo@gmail.com>
    
    * Change wording
    
    Co-authored-by: Michael Corcoran <tracer@outlook.co.nz>
    
    * fix from nut
    
    Co-authored-by: NUT <87286435+NUTTTTTTTT@users.noreply.github.com>
    
    * extra space for formatting
    
    * Another fix by nut
    
    Co-authored-by: NUT <87286435+NUTTTTTTTT@users.noreply.github.com>
    
    * More fixes by nut
    
    Co-authored-by: NUT <87286435+NUTTTTTTTT@users.noreply.github.com>
    
    Co-authored-by: oim1 <oim.oom.imo@gmail.com>
    Co-authored-by: Michael Corcoran <tracer@outlook.co.nz>
    Co-authored-by: NUT <87286435+NUTTTTTTTT@users.noreply.github.com>

---
+++ src/commands/a32nx/takeoffPerf.ts
@@ -0,0 +1,17 @@
+import { CommandDefinition } from '../../lib/command';
+import { CommandCategory } from '../../constants';
+import { makeEmbed, makeLines } from '../../lib/embed';
+
+export const takeoffPerf: CommandDefinition = {
+    name: ['takeoff', 'calculator', 'perf'],
+    description: 'Provides an explanation as to why there is no takeoff calculator for V-speeds or FLEX.',
+    category: CommandCategory.A32NX,
+    executor: (msg) => msg.channel.send(makeEmbed({
+        title: 'FlyByWire A32NX | Where is the takeoff calculator?',
+        description: makeLines(['Currently, Airbus does not publicly release takeoff performance data for the A320neo, which means it is difficult to create an accurate takeoff calculator.', 
+        '', 
+        'While there are many online calculators available, these often use A320ceo data, and are not accurate.' ,
+        '', 
+        'This unfortunately means that we cannot provide a reliable and accurate takeoff performance calculator at this time.']),
+    })),
+};
--- src/commands/index.ts
+++ src/commands/index.ts
@@ -89,6 +89,7 @@ import { ctd } from './support/ctd';
 import { hud } from './support/hud';
 import { fms } from './funnies/fms';
 import { mcdu } from './a32nx/mcdu';
+import { takeoffPerf } from './a32nx/takeoffPerf';
 
 const commands: CommandDefinition[] = [
     ping,
@@ -180,6 +181,7 @@ const commands: CommandDefinition[] = [
     hud,
     fms,
     mcdu,
+    takeoffPerf,
 ];
 
 const commandsObject: { [k: string]: CommandDefinition } = {};
