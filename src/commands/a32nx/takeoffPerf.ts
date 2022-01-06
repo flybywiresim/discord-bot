726730ded1fb8ee5f16318b252d50c2eaf97149f
Sam <93292288+Earthsam12@users.noreply.github.com>
2022-01-06 09:22:32 -0600

    Change wording
    Co-authored-by: Michael Corcoran <tracer@outlook.co.nz>

--- src/commands/a32nx/takeoffPerf.ts
+++ src/commands/a32nx/takeoffPerf.ts
@@ -8,11 +8,11 @@ export const takeoffPerf: CommandDefinition = {
     category: CommandCategory.A32NX,
     executor: (msg) => msg.channel.send(makeEmbed({
         title: 'Where is the takeoff calculator?',
-        description: makeLines(['Currently, Airbus does not publicly release takeoff performance data for the A320neo, which means it is impossible to create an accurate takeoff calculator.', 
+        description: makeLines(['Currently, Airbus does not publicly release takeoff performance data for the A320neo, which means it is difficult to create an accurate takeoff calculator.', 
         ' ', 
         'While there are many online calculators available, these often use A320ceo data, and are not accurate.'
         , 
        ' ', 
-       'This unfortunately means that we cannot provide a reliable and accurate takeoff performance calculator for the A32NX until Airbus releases the data, or we find another way.']),
+       'This unfortunately means that we cannot provide a reliable and accurate takeoff performance calculator at this time.']),
     })),
 };
