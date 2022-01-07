cce059d73d0918eaea89431b14ed8db583a7eea3
Cleon <81078099+xZreai@users.noreply.github.com>
2022-01-07 22:54:27 +0400

    Update src/commands/support/manualleg.ts
    Co-authored-by: Sam <93292288+Earthsam12@users.noreply.github.com>

--- src/commands/support/manualleg.ts
+++ src/commands/support/manualleg.ts
@@ -5,7 +5,7 @@ import { CommandCategory } from '../../constants';
 const MANUAL_URL = 'https://cdn.discordapp.com/attachments/752801628347957248/929071827752480839/mcdu-discontinuity-manual-conceptual.png';
 
 export const manualleg: CommandDefinition = {
-    name: ['manualleg','vm'],
+    name: ['manualleg', 'vm'],
     description: 'Displays image and links to docs about manual waypoints',
     category: CommandCategory.SUPPORT,
     executor: (msg) => msg.channel.send(makeEmbed({ 
