4d9e61415b930c2f2c52ef0e17ec74c488eecad4
Cleon <cleonthegamer@gmail.com>
2022-01-07 22:02:41 +0400

    add new image

--- src/commands/support/manualleg.ts
+++ src/commands/support/manualleg.ts
@@ -2,7 +2,7 @@ import { CommandDefinition } from '../../lib/command';
 import { makeEmbed } from '../../lib/embed';
 import { CommandCategory } from '../../constants';
 
-const MANUAL_URL = 'https://cdn.discordapp.com/attachments/785976111875751956/927657411764969612/unknown.png';
+const MANUAL_URL = 'https://cdn.discordapp.com/attachments/752801628347957248/929071827752480839/mcdu-discontinuity-manual-conceptual.png';
 
 export const manualleg: CommandDefinition = {
     name: ['manualleg','vm'],
