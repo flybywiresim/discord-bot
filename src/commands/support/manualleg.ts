acfb8e6f409e9d17634eceaa2e633e0686467c3f
BenW <bwilliams12@me.com>
2022-01-08 09:25:03 +0000

    Update src/commands/support/manualleg.ts

--- src/commands/support/manualleg.ts
+++ src/commands/support/manualleg.ts
@@ -2,7 +2,7 @@ import { CommandDefinition } from '../../lib/command';
 import { makeEmbed, makeLines } from '../../lib/embed';
 import { CommandCategory } from '../../constants';
 
-const MANUAL_URL = 'https://cdn.discordapp.com/attachments/752801628347957248/929071827752480839/mcdu-discontinuity-manual-conceptual.png';
+const MANUAL_LEG_IMAGE_URL = 'https://cdn.discordapp.com/attachments/752801628347957248/929071827752480839/mcdu-discontinuity-manual-conceptual.png';
 const DISCON_DOCS_URL = 'https://docs.flybywiresim.com/pilots-corner/advanced-guides/flight-planning/disco/?h=manual#discontinuities-after-manual-leg'
 
 export const manualleg: CommandDefinition = {
