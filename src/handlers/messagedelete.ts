5a3a647c89331c6050e2788a8d25bac36ee26de5
Ben Williams <bwilliams12@me.com>
2022-01-10 14:22:12 +0000

    Update messagedelete handler

--- src/handlers/messagedelete.ts
+++ src/handlers/messagedelete.ts
@@ -16,7 +16,8 @@ export const messageDeleted: EventHandlerDefinition<[Message]> = {
         const userLogsChannel = message.guild.channels.resolve(Channels.USER_LOGS) as TextChannel | null;
 
         if (!UserLogExclude.some((e) => e === message.author.id) && userLogsChannel !== null) {
-            await userLogsChannel.send(makeEmbed({
+
+             const messageDeleteEmbed = (makeEmbed({
                 color: 'RED',
                 thumbnail: { url: 'https://cdn.discordapp.com/attachments/770835189419999262/779946282373873694/150-1509174_deleted-message-icon-sign-hd-png-download.png' },
                 author: {
@@ -24,12 +25,13 @@ export const messageDeleted: EventHandlerDefinition<[Message]> = {
                     icon_url: message.author.displayAvatarURL({ dynamic: true }),
                 },
                 fields: [
-                    { name: 'Author', value: message.author, inline: true },
+                    { name: 'Author', value: `\`${message.author}\``, inline: true },
                     { name: 'Channel', value: `<#${message.channel.id}>`, inline: true },
                     { name: 'Deleted Message', value: message.content ? `\`\`\`${message.content}\`\`\`` : FEATURE_NOT_AVAIL, inline: false },
                 ],
                 footer: { text: `User ID: ${message.author.id}` },
             }));
+            await userLogsChannel.send({embeds: [messageDeleteEmbed]});
         }
     },
 };
