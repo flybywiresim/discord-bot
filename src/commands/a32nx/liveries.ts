6bdef1ccd88fa73044ebaa0c8b4080960583bcc2
Ben Williams <bwilliams12@me.com>
2022-01-13 11:30:40 +0000

    Update liveries.ts

--- src/commands/a32nx/liveries.ts
+++ src/commands/a32nx/liveries.ts
@@ -6,8 +6,13 @@ export const liveries: CommandDefinition = {
     name: ['liveries', 'liv'],
     description: 'Provides a link to the flightsim.to A32NX liveries page',
     category: CommandCategory.A32NX,
-    executor: (msg) => msg.channel.send(makeEmbed({
-        title: 'FlyByWire A32NX | Liveries',
-        description: 'You can download liveries for the A32NX [here](https://flightsim.to/c/liveries/flybywire-a32nx/). To install, you will need to unzip the downloaded folder, then place it in your community folder.',
-    })),
+    executor: async (msg) => {
+        const liveriesEmbed = makeEmbed({
+            title: 'FlyByWire A32NX | Liveries',
+            description: 'You can download liveries for the A32NX [here](https://flightsim.to/c/liveries/flybywire-a32nx/). To install, you will need to unzip the downloaded folder, then place it in your community folder.',
+        });
+
+        await msg.channel.send({ embeds: [liveriesEmbed] });
+
+    },
 };
