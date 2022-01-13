45bce239076da63e4fe25af0f5a269a160bb7519
Ben Williams <bwilliams12@me.com>
2022-01-13 11:28:31 +0000

    Update fixinfo.ts

--- src/commands/a32nx/fixinfo.ts
+++ src/commands/a32nx/fixinfo.ts
@@ -8,19 +8,24 @@ export const fixinfo: CommandDefinition = {
     name: 'fixinfo',
     description: 'Provide information about the fix info feature.',
     category: CommandCategory.A32NX,
-    executor: (msg) => msg.channel.send(makeEmbed({
-        title: 'FlyByWire A32NX | Fix Info',
-        description: makeLines([
-            'The A320 comes with a feature called fix info, which allows you to create radii and radials around a waypoint.',
-            '',
-            '**To access the fix info page:**',
-            ' - Go to the MCDU FPLN page,',
-            ' - Select the left LSK of the top most waypoint,',
-            ' - Select the fix info page at the top right.',
-            '',
-            'Check out our [docs](https://docs.flybywiresim.com/pilots-corner/advanced-guides/flight-planning/fixinfo/) for more information on this feature.',
-        ]),
-        image: { url: FIX_INFO_URL },
-        footer: { text: 'Please Note: The computed intercept waypoints and ABEAM functionality are not yet implemented.' },
-    })),
+    executor: async (msg) => {
+        const fixinfoEmbed = makeEmbed({
+            title: 'FlyByWire A32NX | Fix Info',
+            description: makeLines([
+                'The A320 comes with a feature called fix info, which allows you to create radii and radials around a waypoint.',
+                '',
+                '**To access the fix info page:**',
+                ' - Go to the MCDU FPLN page,',
+                ' - Select the left LSK of the top most waypoint,',
+                ' - Select the fix info page at the top right.',
+                '',
+                'Check out our [docs](https://docs.flybywiresim.com/pilots-corner/advanced-guides/flight-planning/fixinfo/) for more information on this feature.',
+            ]),
+            image: { url: FIX_INFO_URL },
+            footer: { text: 'Please Note: The computed intercept waypoints and ABEAM functionality are not yet implemented.' },
+        });
+
+        await msg.channel.send({ embeds: [fixinfoEmbed] });
+
+    },
 };
