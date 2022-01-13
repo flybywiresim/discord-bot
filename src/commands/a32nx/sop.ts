23d58ae98e2951d2d3f493a1014412179b1dc17b
Ben Williams <bwilliams12@me.com>
2022-01-13 11:34:17 +0000

    Update sop.ts

--- src/commands/a32nx/sop.ts
+++ src/commands/a32nx/sop.ts
@@ -8,12 +8,17 @@ export const sop: CommandDefinition = {
     name: 'sop',
     description: 'Displays first page of SOP and provides PDF download',
     category: CommandCategory.A32NX,
-    executor: (msg) => msg.channel.send(makeEmbed({
-        title: 'FlyByWire A32NX | SOP',
-        url: 'https://github.com/flybywiresim/manuals/raw/master/pdf/A32NX%20Documentation/FBW%20A32NX%20SOP.pdf',
-        description: makeLines([
-            'Click the title to download as a PDF',
-        ]),
-        image: { url: SOP_IMAGE_URL },
-    })),
+    executor: async (msg) => {
+        const sopEmbed = makeEmbed({
+            title: 'FlyByWire A32NX | SOP',
+            url: 'https://github.com/flybywiresim/manuals/raw/master/pdf/A32NX%20Documentation/FBW%20A32NX%20SOP.pdf',
+            description: makeLines([
+                'Click the title to download as a PDF',
+            ]),
+            image: { url: SOP_IMAGE_URL },
+        });
+
+        await msg.channel.send({ embeds: [sopEmbed] });
+
+    },
 };
