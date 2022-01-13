a8bcc3a4385ba4333e5f1a9e25d84a5d6dddd486
oim1 <81839029+oim1@users.noreply.github.com>
2022-01-14 01:10:52 +1100

    update addon.ts

--- src/commands/funnies/addon.ts
+++ src/commands/funnies/addon.ts
@@ -1,12 +1,15 @@
-import { CommandDefinition } from '../../lib/command';
-import { makeEmbed } from '../../lib/embed';
-import { CommandCategory } from '../../constants';
+import { CommandDefinition, CommandDefinition } from '../../lib/command';
+import { makeEmbed, makeEmbed, makeLines } from '../../lib/embed';
+import { CommandCategory, CommandCategory } from '../../constants';
 
 const ADDON_URL = 'https://cdn.discordapp.com/attachments/811229810193465344/904049458973646909/meme.png';
 
 export const addon: CommandDefinition = {
     name: 'addon',
-    category: CommandCategory.FUNNIES,
     description: 'addon not mod meme',
-    executor: (msg) => msg.channel.send(makeEmbed({ image: { url: ADDON_URL } })),
+    category: CommandCategory.FUNNIES,
+    executor: async (msg) => {
+        const addonEmbed = makeEmbed({ image: { url: ADDON_URL } });
+        msg.channel.send({ embeds: [addonEmbed] });
+    },
 };
