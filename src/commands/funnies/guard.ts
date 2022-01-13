c61ef31c7c52ce13b61381fefcfb971940528d55
oim1 <81839029+oim1@users.noreply.github.com>
2022-01-14 01:58:59 +1100

    update guard.ts

--- src/commands/funnies/guard.ts
+++ src/commands/funnies/guard.ts
@@ -1,11 +1,15 @@
 import { CommandDefinition } from '../../lib/command';
 import { CommandCategory } from '../../constants';
+import { makeEmbed } from '../../lib/embed';
 
 const GUARD_URL = 'https://tenor.com/view/cat-meow-big-lips-gif-13233291';
 
 export const guard: CommandDefinition = {
     name: 'guard',
-    description: 'MEOW!',
+    description: 'MEOW',
     category: CommandCategory.FUNNIES,
-    executor: (msg) => msg.channel.send( GUARD_URL ),
+    executor: (msg) => {
+        const guardEmbed = makeEmbed({ image: { url: GUARD_URL } });
+        return msg.channel.send({ embeds: [guardEmbed] });
+    },
 };
