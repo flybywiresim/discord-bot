383c1f642387c48a37b81daf1c25520dedd5fbb3
oim1 <81839029+oim1@users.noreply.github.com>
2022-01-14 02:05:31 +1100

    fixed xp.ts

--- src/commands/funnies/xp.ts
+++ src/commands/funnies/xp.ts
@@ -5,8 +5,8 @@ import { makeEmbed } from '../../lib/embed';
 const XP_URL = 'https://tenor.com/view/austin-meyer-austin-slap-annoyed-xplane-gif-17303677';
 
 export const xp: CommandDefinition = {
-    name: 'xp',
-    description: ['xp', 'XPlane', 'X-Plane'],
+    name: ['xp', 'XPlane', 'X-Plane'],
+    description: 'XPlane',
     category: CommandCategory.FUNNIES,
     executor: (msg) => {
         const xpEmbed = makeEmbed({ image: { url: XP_URL } });
