cbdd9f2c04894b66e3c9ebf50a51304a9909b54a
oim1 <81839029+oim1@users.noreply.github.com>
2022-01-14 02:02:39 +1100

    update xp.ts

--- src/commands/funnies/xp.ts
+++ src/commands/funnies/xp.ts
@@ -1,11 +1,15 @@
 import { CommandDefinition } from '../../lib/command';
 import { CommandCategory } from '../../constants';
+import { makeEmbed } from '../../lib/embed';
 
 const XP_URL = 'https://tenor.com/view/austin-meyer-austin-slap-annoyed-xplane-gif-17303677';
 
 export const xp: CommandDefinition = {
-    name: ['xp', 'XPlane', 'X-Plane'],
-    description: 'Just... no!',
+    name: 'xp',
+    description: ['xp', 'XPlane', 'X-Plane'],
     category: CommandCategory.FUNNIES,
-    executor: (msg) => msg.channel.send( XP_URL ),
+    executor: (msg) => {
+        const xpEmbed = makeEmbed({ image: { url: XP_URL } });
+        return msg.channel.send({ embeds: [xpEmbed] });
+    },
 };
