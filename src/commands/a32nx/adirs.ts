c149378e1ae22f5daa8cdde9d29f2ca373d76aac
Ben Williams <bwilliams12@me.com>
2022-01-13 10:44:57 +0000

    Update adirs.ts

--- src/commands/a32nx/adirs.ts
+++ src/commands/a32nx/adirs.ts
@@ -8,12 +8,18 @@ export const adirs: CommandDefinition = {
     name: 'adirs',
     description: 'Display help with ADIRS alignment',
     category: CommandCategory.A32NX,
-    executor: (msg) => msg.channel.send(makeEmbed({
-        title: 'FlyByWire A32NX | ADIRS align',
-        description: makeLines([
-            'On the overhead panel you will see the three switches under \'ADIRS\'. Turn these three to the \'NAV\' position. It takes several minutes for the ADIRUs to align.',
-            'You can check how long you have to wait by looking at the align time on your Upper Ecam.',
-        ]),
-        image: { url: ADIRS_IMAGE_URL },
-    })),
+    executor: async (msg) => {
+
+        const adirsEmbed = makeEmbed({
+            title: 'FlyByWire A32NX | ADIRS align',
+            description: makeLines([
+                'On the overhead panel you will see the three switches under \'ADIRS\'. Turn these three to the \'NAV\' position. It takes several minutes for the ADIRUs to align.',
+                'You can check how long you have to wait by looking at the align time on your Upper Ecam.',
+            ]),
+            image: { url: ADIRS_IMAGE_URL },
+        });
+
+        await msg.channel.send({ embeds: [adirsEmbed] });
+
+    },
 };
