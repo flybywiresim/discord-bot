f3929a6f928ef6269ace4d9340e0ae6bc6bc0a11
oim1 <81839029+oim1@users.noreply.github.com>
2022-01-15 01:51:27 +1100

    updated messagedelete handler to include deleted images

--- src/handlers/messagedelete.ts
+++ src/handlers/messagedelete.ts
@@ -3,8 +3,6 @@ import { EventHandlerDefinition } from '../lib/handler';
 import { Channels, UserLogExclude } from '../constants';
 import { makeEmbed } from '../lib/embed';
 
-const FEATURE_NOT_AVAIL = '(can\'t show embeds or images)';
-
 export const messageDeleted: EventHandlerDefinition<[Message]> = {
     event: 'messageDelete',
     executor: async (message) => {
@@ -16,20 +14,52 @@ export const messageDeleted: EventHandlerDefinition<[Message]> = {
         const userLogsChannel = message.guild.channels.resolve(Channels.USER_LOGS) as TextChannel | null;
 
         if (!UserLogExclude.some((e) => e === message.author.id) && userLogsChannel !== null) {
-            await userLogsChannel.send(makeEmbed({
-                color: 'RED',
-                thumbnail: { url: 'https://cdn.discordapp.com/attachments/770835189419999262/779946282373873694/150-1509174_deleted-message-icon-sign-hd-png-download.png' },
-                author: {
-                    name: message.author.tag,
-                    icon_url: message.author.displayAvatarURL({ dynamic: true }),
-                },
-                fields: [
-                    { name: 'Author', value: message.author, inline: true },
-                    { name: 'Channel', value: `<#${message.channel.id}>`, inline: true },
-                    { name: 'Deleted Message', value: message.content ? `\`\`\`${message.content}\`\`\`` : FEATURE_NOT_AVAIL, inline: false },
-                ],
-                footer: { text: `User ID: ${message.author.id}` },
-            }));
+            if (message.attachments.size > 0 && message.content) {
+                await userLogsChannel.send(makeEmbed({
+                    color: 'RED',
+                    thumbnail: { url: 'https://cdn.discordapp.com/attachments/770835189419999262/779946282373873694/150-1509174_deleted-message-icon-sign-hd-png-download.png' },
+                    author: {
+                        name: message.author.tag,
+                        icon_url: message.author.displayAvatarURL({ dynamic: true }),
+                    },
+                    fields: [
+                        { name: 'Author', value: message.author, inline: true },
+                        { name: 'Channel', value: `<#${message.channel.id}>`, inline: true },
+                        { name: 'Deleted message', value: message.content, inline: false },
+                        { name: 'Deleted Image', value: message.attachments.first().url, inline: false },
+                    ],
+                    footer: { text: `User ID: ${message.author.id}` },
+                }));
+            } else if (message.attachments.size > 0) {
+                await userLogsChannel.send(makeEmbed({
+                    color: 'RED',
+                    thumbnail: { url: 'https://cdn.discordapp.com/attachments/770835189419999262/779946282373873694/150-1509174_deleted-message-icon-sign-hd-png-download.png' },
+                    author: {
+                        name: message.author.tag,
+                        icon_url: message.author.displayAvatarURL({ dynamic: true }),
+                    },
+                    fields: [
+                        { name: 'Author', value: message.author, inline: true },
+                        { name: 'Channel', value: `<#${message.channel.id}>`, inline: true },
+                        { name: 'Deleted Message', value: message.attachments.first().url, inline: false },
+                    ],
+                    footer: { text: `User ID: ${message.author.id}` },
+                }));
+            } else {
+                await userLogsChannel.send(makeEmbed({
+                    color: 'RED',
+                    thumbnail: { url: 'https://cdn.discordapp.com/attachments/770835189419999262/779946282373873694/150-1509174_deleted-message-icon-sign-hd-png-download.png' },
+                    author: {
+                        name: message.author.tag,
+                        icon_url: message.author.displayAvatarURL({ dynamic: true }),
+                    },
+                    fields: [
+                        { name: 'Author', value: message.author, inline: true },
+                        { name: 'Channel', value: `<#${message.channel.id}>`, inline: false},
+                        { name: 'Deleted Message', value: message.content, inline: false },
+                    ]
+                }))
+            }
         }
     },
 };
