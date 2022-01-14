121eeb6370ecf241467abd88c0b307f9c404fc1f
Ben Williams <bwilliams12@me.com>
2022-01-14 18:19:35 +0000

    Updated hud.ts

--- src/commands/support/hud.ts
+++ src/commands/support/hud.ts
@@ -6,9 +6,14 @@ export const hud: CommandDefinition = {
     name: 'hud',
     description: 'How to disable in-game HUD',
     category: CommandCategory.SUPPORT,
-    executor: (msg) => msg.channel.send(makeEmbed({
-        title: 'FlyByWire Support | Disable in game HUD',
-        description: 'Please turn off \'INSTRUMENTS HEADS UP DISPLAY - CHASE CAM\' and \'COCKPIT CAM\' in Settings > Assistance Options > User Experience ',
-        image: {url:'https://cdn.discordapp.com/attachments/752801628347957248/927195966547824710/unknown.png'}
-    })),
+    executor: async (msg) => {
+        const hudEmbed = makeEmbed({
+            title: 'FlyByWire Support | Disable in game HUD',
+            description: 'Please turn off \'INSTRUMENTS HEADS UP DISPLAY - CHASE CAM\' and \'COCKPIT CAM\' in Settings > Assistance Options > User Experience ',
+            image: {url:'https://cdn.discordapp.com/attachments/752801628347957248/927195966547824710/unknown.png'}
+        });
+
+        await msg.channel.send({ embeds: [hudEmbed] });
+
+    },
 };
