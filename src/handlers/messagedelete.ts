2faf7d9ee968cab63d731e470b243896da0b6e80
oim1 <oim.oom.imo@gmail.com>
2022-01-15 18:41:03 +1100

    fix: messagedelete handler now includes deleted images (#148)
    * updated messagedelete handler to include deleted images
    
    * fixed wording
    
    * removed unnecessary embeds

--- src/handlers/messagedelete.ts
+++ src/handlers/messagedelete.ts
@@ -3,8 +3,6 @@ import { EventHandlerDefinition } from '../lib/handler';
 import { Channels, UserLogExclude } from '../constants';
 import { makeEmbed } from '../lib/embed';
 
-const FEATURE_NOT_AVAIL = '(can\'t show embeds or images)';
-
 export const messageDeleted: EventHandlerDefinition<[Message]> = {
     event: 'messageDelete',
     executor: async (message) => {
@@ -16,7 +14,7 @@ export const messageDeleted: EventHandlerDefinition<[Message]> = {
         const userLogsChannel = message.guild.channels.resolve(Channels.USER_LOGS) as TextChannel | null;
 
         if (!UserLogExclude.some((e) => e === message.author.id) && userLogsChannel !== null) {
-            await userLogsChannel.send(makeEmbed({
+            const deleteEmbed = makeEmbed({
                 color: 'RED',
                 thumbnail: { url: 'https://cdn.discordapp.com/attachments/770835189419999262/779946282373873694/150-1509174_deleted-message-icon-sign-hd-png-download.png' },
                 author: {
@@ -26,10 +24,21 @@ export const messageDeleted: EventHandlerDefinition<[Message]> = {
                 fields: [
                     { name: 'Author', value: message.author, inline: true },
                     { name: 'Channel', value: `<#${message.channel.id}>`, inline: true },
-                    { name: 'Deleted Message', value: message.content ? `\`\`\`${message.content}\`\`\`` : FEATURE_NOT_AVAIL, inline: false },
                 ],
                 footer: { text: `User ID: ${message.author.id}` },
-            }));
+            });
+
+            if (message.attachments.size > 0 && message.content) {
+                deleteEmbed.fields[2] = { name: 'Deleted Message', value: message.content, inline: false };
+                deleteEmbed.fields[3] = { name: 'Deleted Image', value: message.attachments.first().url, inline: false };
+                await userLogsChannel.send(deleteEmbed);
+            } else if (message.attachments.size > 0) {
+                deleteEmbed.fields[2] = { name: 'Deleted Image', value: message.attachments.first().url, inline: false };
+                await userLogsChannel.send(deleteEmbed);
+            } else {
+                deleteEmbed.fields[2] = { name: 'Deleted Message', value: message.content, inline: false };
+                await userLogsChannel.send(deleteEmbed);
+            }
         }
     },
 };
