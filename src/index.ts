83b9bd71a4cfbcc34641ff8949e104db690fbca1
Ben Williams <bwilliams12@me.com>
2022-01-17 17:27:05 +0000

    Add DM not sent embed

--- src/index.ts
+++ src/index.ts
@@ -80,6 +80,13 @@ client.on('message',async (msg) => {
                 await msg.author.send('This function is in place to prevent discord scams. This has resulted in an auto mute and notification of our moderation team. If this was done in error, our moderation team will reverse the mute, however please refrain from using the @everyone ping in future.');
             } catch (e) {
                 Logger.error(e);
+                await (client.channels.cache.find((channel) => channel.id === '931928312303976488') as TextChannel).send(makeEmbed({
+                    author: {
+                        name: msg.author.tag,
+                        icon_url: msg.author.displayAvatarURL({ dynamic: true }),
+                    },
+                    description: ' DM was not sent to ' + `<@${  msg.author.id  }>` + '.',
+                }));
             }
             await (client.channels.cache.find((channel) => channel.id === '931928312303976488') as TextChannel).send(makeEmbed({
                 title: 'Potential Scam Alert',
