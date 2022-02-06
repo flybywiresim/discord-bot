85ddb4417765c3f9c746ddf6c842eb0632bb15e0
NUT <87286435+NUTTTTTTTT@users.noreply.github.com>
2022-02-06 18:46:40 +0000

    Update src/commands/a32nx/CPDLC.ts
    Co-authored-by: BenW <bwilliams12@me.com>

--- src/commands/a32nx/CPDLC.ts
+++ src/commands/a32nx/CPDLC.ts
@@ -14,6 +14,6 @@ export const CPDLC: CommandDefinition = {
             '',
             '**Check out our [docs](https://docs.flybywiresim.com/fbw-a32nx/feature-guides/hoppie/) to learn how to get started!**',
         ]),
-        footer: { text: 'Please Note: The Hoppie ACARS system is a complex system and we have decided to develop and release it step-by-step. Not all features are available in the early releases and additional functionality will be added over time. '},
+        footer: { text: 'Please Note: The Hoppie ACARS system is a complex system and we have decided to develop and release it step-by-step. Not all features are available in the early releases and additional functionality will be added over time.'},
     })),
 };
