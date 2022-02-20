2e62f0f8493eaf667fe9190e99e1f2577a1add15
Ben Williams <bwilliams12@me.com>
2022-02-20 20:57:28 +0000

    Fix whois.ts

---
+++ src/commands/moderation/whois.ts
@@ -0,0 +1,67 @@
+import moment from 'moment';
+import { CommandDefinition } from '../../lib/command';
+import { CommandCategory } from '../../constants';
+import { makeEmbed } from '../../lib/embed';
+
+enum beautifiedStatus {
+    ONLINE = "Online",
+    IDLE = "Idle",
+    DND = "Do Not Disturb",
+    OFFLINE ="Offline"
+}
+
+export const whois: CommandDefinition = {
+    name: 'whois',
+    description: 'Provides an embedded message with information about the mentioned user',
+    category: CommandCategory.MODERATION,
+    executor: async (msg) => {
+        let query = msg.content.replace(/\.whois(\s|$)+/, '').replace(/[@#!<>]+/g, '');
+        let targetMember = query ? await msg.guild.members.fetch(query) : msg.member;
+
+        const filteredRoles = targetMember.roles.cache.filter(role => role.id != msg.guild.id);
+        const listedRoles = filteredRoles.sort((a, b) => b.position - a.position).map(role => role.toString());
+
+            const whoisEmbed = makeEmbed({
+            author: {
+                name: targetMember.user.username,
+                icon_url: targetMember.user.avatarURL(),
+            },
+            description: `<@!${targetMember.id}>`,
+            thumbnail: {
+                url: targetMember.user.avatarURL()
+            },
+            fields: [
+                {
+                    name: "Username",
+                    value: targetMember.user.tag,
+                    inline: true
+                },
+                {
+                    name: "Status",
+                    value: beautifiedStatus[targetMember.presence.status.toUpperCase()],
+                    inline: true
+                },
+                {
+                    name: "Joined",
+                    value: moment(targetMember.joinedTimestamp).format("llll"),
+                    inline: true
+                },
+                {
+                    name: "Registered",
+                    value: moment(targetMember.user.createdTimestamp).format("llll"),
+                    inline: false
+                },
+                {
+                    name: "Roles",
+                    value: '\u200B' + listedRoles.join(","),
+                },
+                {
+                    name: "Permissions",
+                    value: targetMember.permissions.toArray().join(", ").toLowerCase().replace(/_/g, " ").replace(/(^\w{1})|(\s+\w{1})/g, char => char.toUpperCase())
+                }
+            ]
+        })
+
+        return msg.channel.send({ embeds: [whoisEmbed] });
+    }
+};
