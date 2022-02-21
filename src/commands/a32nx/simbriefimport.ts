be0db47a8f152ced104fba10bc735a22f25802b4
nebula231 <88508303+nebula231@users.noreply.github.com>
2022-02-21 09:13:02 -0600

    Update src/commands/a32nx/simbriefimport.ts
    Co-authored-by: BenW <bwilliams12@me.com>

--- src/commands/a32nx/simbriefimport.ts
+++ src/commands/a32nx/simbriefimport.ts
@@ -11,7 +11,7 @@ export const simbriefimport: CommandDefinition = {
     executor: (msg) => msg.channel.send(makeEmbed({
         title: 'FlyByWire A32NX | SimBrief import',
         description: makeLines([
-            'The A32NX has SimBrief integration, and you can create a flight plan in SimBrief and import it into the MCDU. There are some steps involved in doing this, so please refer to our documentation on more information. https://docs.flybywiresim.com/pilots-corner/beginner-guide/preflight/#flight-plan-import (The image below is how it will look once everything has been set up properly)'
+            'The A32NX has SimBrief integration. This means you can create a flight plan in SimBrief and import it into the MCDU! Please refer to our [documentation](https://docs.flybywiresim.com/pilots-corner/beginner-guide/preflight/#flight-plan-import) for instructions. (The image below is how it will look once everything has been set up properly)',
         ]),
         image: { url: SIMBRIEF_INTEGRATION_IMAGE_URL},
     })),
