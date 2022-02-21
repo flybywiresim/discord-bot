97e8a94866cf94c26540270f60fbf7512e5f0304
Benjamin Dupont <benjozorkfr@gmail.com>
2022-02-21 13:42:38 -0500

    fix: simBrief command cleanup (#155)
    * fix: simBrief command cleanup
    
    * fix

--- src/commands/a32nx/simbriefimport.ts
+++ src/commands/a32nx/simbriefimport.ts
@@ -7,9 +7,9 @@ export const simbriefimport: CommandDefinition = {
     description: 'Shows how to use SimBrief integration',
     category: CommandCategory.A32NX,
     executor: (msg) => msg.channel.send(makeEmbed({
-        title: 'FlyByWire A32NX | SimBrief import',
+        title: 'FlyByWire A32NX | simBrief import',
         description: makeLines([
-            'The A32NX has SimBrief integration. This means you can create a flight plan in SimBrief and import it into the MCDU! Please refer to our [documentation](https://docs.flybywiresim.com/pilots-corner/beginner-guide/preflight/#flight-plan-import) for instructions.',
+            'The A32NX has simBrief integration. This means you can create a flight plan in simBrief and import it into the FMS. Please refer to our [documentation](https://docs.flybywiresim.com/pilots-corner/beginner-guide/preflight/#flight-plan-import) for instructions.',
         ]),
     })),
 };
