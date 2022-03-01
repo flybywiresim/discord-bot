de3693b5961e070e793ea296796a71437021a141
Sam <93292288+Earthsam12@users.noreply.github.com>
2022-03-01 17:18:28 -0500

    clean up embed part 2: electric boogaloo

--- src/commands/moderation/timeout.ts
+++ src/commands/moderation/timeout.ts
@@ -1,6 +1,7 @@
 import { CommandDefinition } from '../../lib/command';
 import { CommandCategory } from '../../constants';
 import { makeEmbed } from '../../lib/embed';
+import { GuildMember } from 'discord.js';
 
 enum TimeConversions {
     MINUTES_TO_MILLISECONDS = 60 * 1000,
@@ -13,11 +14,34 @@ const DMEmbed = () => makeEmbed({
     // stuff
 });
 
-const modLogEmbed = () => makeEmbed({
-    // sutff
+const timeoutEmbed = (user: GuildMember, reason: string, timeoutLength: string) => makeEmbed({
+    title: 'User Successfully Timed Out',
+    fields: [
+        {
+            inline: true,
+            name: 'User',
+            value: user.toString(),
+        },
+        {
+            inline: true,
+            name: 'ID',
+            value: user.id,
+        },
+        {
+            inline: false,
+            name: 'Reason',
+            value: reason,
+        },
+        {
+            inline: true, // TODO: move this field to the second row somehow?
+            name: 'Timeout Length',
+            value: timeoutLength,
+        },
+    ],
+    color: 'GREEN',
 });
 
-const timeoutEmbed = () => makeEmbed({
+const modLogEmbed = () => makeEmbed({
     // stuff
 });
 
@@ -35,13 +59,13 @@ export const timeout: CommandDefinition = {
         const id = args[0];
         const user = await msg.guild.members.fetch(id);
         const timeoutArg = args[1];
-        const reason = args[2];
+        const reason = args.slice(2).join(' ');
 
         let timeoutLength: number;
-        switch (timeoutArg[timeoutArg.length-1]) {
+        switch (timeoutArg[timeoutArg.length - 1]) {
             default: {
                 // defaults to minutes; 'm' will also run this block
-                timeoutLength = parseInt(timeoutArg.slice(0, timeoutArg.length - 1)) * TimeConversions.MINUTES_TO_MILLISECONDS;
+                timeoutLength = parseInt(timeoutArg.replace('m', '')) * TimeConversions.MINUTES_TO_MILLISECONDS;
                 break;
             }
             case 'h': {
@@ -60,30 +84,6 @@ export const timeout: CommandDefinition = {
 
         await user.timeout(timeoutLength, reason);
         // todo: check if successful
-        await msg.channel.send(
-            {
-                embeds: [makeEmbed({
-                    title: 'User Successfully Timed Out',
-                    fields: [
-                        {
-                            inline: true,
-                            name: 'User',
-                            value: user.toString(),
-                        },
-                        {
-                            inline: true,
-                            name: 'ID',
-                            value: id,
-                        },
-                        {
-                            inline: false,
-                            name: 'Reason',
-                            value: reason,
-                        },
-                    ],
-                    color: 'GREEN',
-                })],
-            },
-        );
+        await msg.channel.send({ embeds: [timeoutEmbed(user, reason, timeoutArg)] });
     },
 };
