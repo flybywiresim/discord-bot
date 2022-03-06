f9f98ee48e0439690d0096e6a6bb11a14f7ba249
Valastiri <reyes.nico@gmail.com>
2022-03-06 15:22:07 +0800

    refactor: update commands that reference status of experimental branch

--- src/commands/a32nx/experimental.ts
+++ src/commands/a32nx/experimental.ts
@@ -1,6 +1,6 @@
 import { CommandDefinition } from '../../lib/command';
 import { CommandCategory } from '../../constants';
-import { makeEmbed } from '../../lib/embed';
+import { makeEmbed, makeLines } from '../../lib/embed';
 
 export const experimental: CommandDefinition = {
     name: ['experimental', 'exp'],
@@ -9,8 +9,11 @@ export const experimental: CommandDefinition = {
     executor: async (msg) => {
         const experimentalEmbed = makeEmbed({
             title: 'FlyByWire A32NX | Experimental Version',
-            description: 'We are currently testing updates to our custom FMS LNAV, and other additional improvements. Please see our [Experimental Version Support Page](https://docs.flybywiresim.com/fbw-a32nx/support/exp/) for more information. **No support will be offered via Discord.** '
-                + 'The Experimental version is a test version to find problems and issues and to improve functionality based on your feedback. It is not meant to be used for daily use or when you try to do a serious flight on an Online ATC service. '
+            description: makeLines([
+                'Currently experimental is undergoing updates that can break the aircraft and is not 100% ready for public use. All previous features have been moved to our development version. Please see our [Experimental Version Support Page](https://docs.flybywiresim.com/fbw-a32nx/support/exp/) for more information. **No support will be offered via Discord.** ',
+                '',
+                'The Experimental version is a test version to find problems and issues and to improve functionality based on your feedback. It is not meant to be used for daily use or when you try to do a serious flight on an Online ATC service. ',
+            ]),
         });
 
         await msg.channel.send({ embeds: [experimentalEmbed] });
