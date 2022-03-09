981cd626e140d55146a8d596834cc320c1248254
NUTTTTTTTT <Haldenbyc@gmail.com>
2022-03-09 20:13:52 +0000

    feat: add role assignment messsages

---
+++ src/commands/moderation/roleassignment.ts
@@ -0,0 +1,39 @@
+import { CommandDefinition } from '../../lib/command';
+import { makeEmbed, makeLines } from '../../lib/embed';
+import { CommandCategory } from '../../constants';
+
+const ROLES_EMBED =
+    makeEmbed({
+        title: 'Role Assignment',
+        description: makeLines([
+            'Please react below to set your role according to your skill set. If you do not have the skills in any of the available roles, please do not react as this will not benefit the development of the addon.',
+            '',
+            ':computer: Interested in Programming',
+            ':triangular_ruler: Interested in Modelling/Design',
+        ]),
+    });
+
+const MEDIA_ANNOUNCEMENT_EMBED =
+    makeEmbed({
+        title: 'Media Announcements',
+        description: makeLines([
+            'Please react to the corresponding reactions to be pinged for the various announcements.',
+            '',
+            ':mega: Server Announcements',
+            ':airplane: A320 Releases',
+            ':hourglass_flowing_sand: Progress Updates',
+            ':newspaper: Media Announcements (FBW YouTube)',
+        ]),
+    });
+
+
+export const roleassignment: CommandDefinition = {
+    name: 'roleassignment',
+    description: 'Sends the role assignment messages',
+    requiredPermissions: ['BAN_MEMBERS'],
+    category: CommandCategory.MODERATION,
+    executor: async (msg) => {
+        await msg.channel.send({ embeds: [ROLES_EMBED] });
+        await msg.channel.send({ embeds: [MEDIA_ANNOUNCEMENT_EMBED] });
+    },
+};
