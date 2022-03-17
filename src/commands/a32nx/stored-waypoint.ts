36e80201eb9918fce76b1d38ef604927b467988c
Ben Williams <bwilliams12@me.com>
2022-03-17 21:12:31 +0000

    update stored-waypoint.ts

--- src/commands/a32nx/stored-waypoint.ts
+++ src/commands/a32nx/stored-waypoint.ts
@@ -3,13 +3,13 @@ import { CommandCategory } from '../../constants';
 import { makeEmbed } from '../../lib/embed';
 
 export const storedWaypoint: CommandDefinition = {
-    name: ['waypoint', 'customwaypoint'],
-    description: 'Provides a link to the a32nx custom waypoint guide',
+    name: ['waypoint', 'storedwaypoint', 'datamanagement'],
+    description: 'Provides a link to the a32nx data management guide',
     category: CommandCategory.A32NX,
     executor: async (msg) => {
         const waypointEmbed = makeEmbed({
             title: 'FlyByWire A32NX | Stored Waypoints',
-            description: 'Please see our [guide](https://docs.flybywiresim.com/pilots-corner/advanced-guides/data-management/) stored waypoints.',
+            description: 'Please see our [guide](https://docs.flybywiresim.com/pilots-corner/advanced-guides/data-management/) on stored waypoints.',
         });
 
         await msg.channel.send({ embeds: [waypointEmbed] });
