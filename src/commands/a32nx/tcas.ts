c79647529922115be8be974eb78581ed99976046
Valastiri <reyes.nico@gmail.com>
2022-03-21 04:00:54 +0800

    feat: tcas command

---
+++ src/commands/a32nx/tcas.ts
@@ -0,0 +1,26 @@
+import { CommandDefinition } from '../../lib/command';
+import { CommandCategory } from '../../constants';
+import { makeEmbed, makeLines } from '../../lib/embed';
+
+export const tcas: CommandDefinition = {
+    name: 'tcas',
+    description: 'Provides support information about A32NX AP/FD TCAS',
+    category: CommandCategory.A32NX,
+    executor: async (msg) => {
+        const tcasEmbed = makeEmbed({
+            title: 'FlyByWire A32NX | AP/FD TCAS',
+            description: makeLines([
+                'The A32NX is capable of handling TA/RAs while on an ATC network such as VATSIM/IVAO '
+                + 'or with the live traffic feature in MSFS. ',
+                '',
+                'Our implementation utilizes AP/FD TCAS by Airbus which allows for Autopilot controlled corrective maneuvers. ',
+                '',
+                'For details on how the system functions, known issues, and pilot actions see our '
+                + '[**Advanced Guide: TCAS**](https://docs.flybywiresim.com/pilots-corner/advanced-guides/flight-guidance/tcas/). ',
+             ]),
+        });
+
+        await msg.channel.send({ embeds: [tcasEmbed] });
+
+    },
+};
