10b05fe7a521d8eb01a8f65845ef81f5cbc312ca
oim1 <oim.oom.imo@gmail.com>
2022-03-23 00:53:59 +1100

    docs: update .experimental to reflect the new changes to the version (#176)
    * updated .experimental to include new updates
    
    * update versions
    
    * fix link in .versions
    
    * Update src/commands/a32nx/experimental.ts
    
    * Update src/commands/a32nx/versions.ts
    
    Co-authored-by: BenW <bwilliams12@me.com>

--- src/commands/a32nx/experimental.ts
+++ src/commands/a32nx/experimental.ts
@@ -10,9 +10,9 @@ export const experimental: CommandDefinition = {
         const experimentalEmbed = makeEmbed({
             title: 'FlyByWire A32NX | Experimental Version',
             description: makeLines([
-                'Currently experimental is undergoing updates that can break the aircraft and is not 100% ready for public use. All previous features have been moved to our development version. Please see our [Experimental Version Support Page](https://docs.flybywiresim.com/fbw-a32nx/support/exp/) for more information. **No support will be offered via Discord.** ',
+                'Currently experimental is geared toward testing the initial version of VNAV. Please use the appropriate discord thread <#926586416820011098> to discuss any issues. Please see our [Experimental Version Support Page](https://docs.flybywiresim.com/fbw-a32nx/support/exp/) for more information. **No support will be offered via Discord.** ',
                 '',
-                'The Experimental version is a test version to find problems and issues and to improve functionality based on your feedback. It is not meant to be used for daily use or when you try to do a serious flight on an Online ATC service. ',
+                'The Experimental version is a test version to find problems, issues and to improve functionality based on your feedback. It is not meant to be used for daily use or serious flights with an Online ATC service. ',
             ]),
         });
 
