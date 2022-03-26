17bf48f7f1d0e8396fd1030ff76a1c8680c942b5
Hypeism <jhnwllms236@gmail.com>
2022-03-26 18:49:17 -0400

    object fixes

--- src/commands/birthdays/birthday.ts
+++ src/commands/birthdays/birthday.ts
@@ -28,8 +28,6 @@ export const birthday: CommandDefinition = {
                 });
             await msg.channel.send({ embeds: [errorEmbed] });
 
-        },
-
         const dateString = `${args[1]} ${args[0]} ${args[2]}`;
 
         const date = new Date(dateString);
@@ -46,26 +44,30 @@ export const birthday: CommandDefinition = {
             await dbServer.save();
 
             const editEmbed = makeEmbed( {
-               title: 'Birthday Edited',
+                title: 'Birthday Edited',
                 description:`Your birthday is modified to ${date}`,
             })
             await msg.channel.send({ embeds: [editEmbed] });
         } else {
             const newBirthday = new Member({
-                user: message.author.tag,
-                birthday: date,
-                discord_id: message.author.id,
-            }),
-            dbServer.members.push(newBirthday),
-            dbServer.markModified('members'),
-            await dbServer.save(),
+                    user: message.author.tag,
+                    birthday: date,
+                    discord_id: message.author.id,
+                }),
+                dbServer.members.push(newBirthday),
+                dbServer.markModified('members'),
+                await dbServer.save(),
 
             const setEmbed = makeEmbed( {
-                title: 'Birthday is Set',
-                description: `Your birthday has been successfully set to ${date}`,
-            },
+                    title: 'Birthday is Set',
+                    description: `Your birthday has been successfully set to ${date}`,
+                });
 
                 await msg.channel.send({ embeds: [setEmbed] });
         }
     }
 };
+
+
+
+
