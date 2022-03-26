21de55e22c71e058f40ea33950a426e17d700117
Valastiri <reyes.nico@gmail.com>
2022-03-26 23:06:40 +0800

    refactor: experimental command info + copy layout

--- src/commands/a32nx/experimental.ts
+++ src/commands/a32nx/experimental.ts
@@ -10,7 +10,11 @@ export const experimental: CommandDefinition = {
         const experimentalEmbed = makeEmbed({
             title: 'FlyByWire A32NX | Experimental Version',
             description: makeLines([
-                'Currently experimental is geared toward testing the initial version of VNAV. Please use the appropriate discord thread <#926586416820011098> to discuss any issues. Please see our [Experimental Version Support Page](https://docs.flybywiresim.com/fbw-a32nx/support/exp/) for more information. **No support will be offered via Discord.** ',
+                'Currently experimental is geared toward testing the initial version of VNAV and flyPadOS3. Please see our [Experimental Version Support Page](https://docs.flybywiresim.com/fbw-a32nx/support/exp/) for more information. **No support will be offered via Discord.** ',
+                '',
+                'Please use the appropriate discord thread to discuss any issues: ',
+                '- <#926586416820011098> ',
+                '- <#957208413887139860> ',
                 '',
                 'The Experimental version is a test version to find problems, issues and to improve functionality based on your feedback. It is not meant to be used for daily use or serious flights with an Online ATC service. ',
             ]),
