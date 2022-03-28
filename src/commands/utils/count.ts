f70d2ad2de450ecfc6c4d876093faaf110cd419e
oim1 <81839029+oim1@users.noreply.github.com>
2022-03-29 00:02:51 +1100

    added thread to constants

--- src/commands/utils/count.ts
+++ src/commands/utils/count.ts
@@ -1,22 +1,23 @@
 import { TextChannel } from 'discord.js';
 import { CommandDefinition } from '../../lib/command';
-import { CommandCategory } from '../../constants';
-
-const COUNT_CHANNEL_ID = '877049017102659654';
+import { CommandCategory, Channels } from '../../constants';
 
 export const count: CommandDefinition = {
     name: 'count',
     description: 'counts in the count thread',
     category: CommandCategory.UTILS,
     executor: async (msg) => {
+        if (msg.client.channels.cache.get(Channels.COUNT_THREAD) as TextChannel === undefined) {
+            return msg.reply('That channel no longer exists');
+        }
         if (!msg.member.roles.cache.some((role) => role.name === 'Bot Developer')) {
-            return msg.reply('Missing required role: Bot Developer');
+            return msg.reply('Go count yourself');
         }
         const countNumber = msg.content.replace(/\.count\s*/, '');
         if (Number.isNaN(parseInt(countNumber))) {
             await msg.reply('Please provide a number');
         } else if (countNumber) {
-            await (msg.client.channels.cache.get(COUNT_CHANNEL_ID) as TextChannel).send(`<@${msg.author}> says ${countNumber}`);
+            await (msg.client.channels.cache.get(Channels.COUNT_THREAD) as TextChannel).send(`<@${msg.author}> says ${countNumber}`);
         }
     },
 };
--- src/constants.ts
+++ src/constants.ts
@@ -15,6 +15,7 @@ export enum Channels {
     MOD_LOGS = '783996780181585921',
     USER_LOGS = '779944761699729418',
     SCAM_LOGS = '932687046315737149',
+    COUNT_THREAD = '877049017102659654',
 }
 
 export const UserLogExclude = [
