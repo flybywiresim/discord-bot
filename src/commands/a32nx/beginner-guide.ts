bc45e902c4b084a883770b94562800ba103b3c4d
Valastiri <reyes.nico@gmail.com>
2022-03-30 07:08:56 +0800

    feat: more detailed beginner guide command

--- src/commands/a32nx/beginner-guide.ts
+++ src/commands/a32nx/beginner-guide.ts
@@ -1,6 +1,6 @@
 import { CommandDefinition } from '../../lib/command';
 import { CommandCategory } from '../../constants';
-import { makeEmbed } from '../../lib/embed';
+import { makeEmbed, makeLines } from '../../lib/embed';
 
 export const beginner: CommandDefinition = {
     name: ['beginnerguide', 'bg'],
@@ -9,7 +9,21 @@ export const beginner: CommandDefinition = {
     executor: async (msg) => {
         const beginnerEmbed = makeEmbed({
             title: 'FlyByWire A32NX | Beginner\'s Guide',
-            description: 'Please see our [beginner\'s guide](https://docs.flybywiresim.com/pilots-corner/beginner-guide/overview/) for information on how to set up and fly the A32NX.',
+            description: makeLines ([
+                'Please see our [beginner\'s guide](https://docs.flybywiresim.com/pilots-corner/beginner-guide/overview/) for information on how to set up and fly the A32NX.',
+                '',
+                'If you\'d like to immediately go to a specific chapter please use the list below:',
+                '- [Preflight](https://docs.flybywiresim.com/pilots-corner/beginner-guide/preflight/)',
+                '- [Starting the Aircraft](https://docs.flybywiresim.com/pilots-corner/beginner-guide/starting-the-aircraft/)',
+                '- [Preparing the MCDU](https://docs.flybywiresim.com/pilots-corner/beginner-guide/preparing-mcdu/)',
+                '- [Engine Start and Taxi](https://docs.flybywiresim.com/pilots-corner/beginner-guide/engine-start-taxi/)',
+                '- [Takeoff, Climb and Cruise](https://docs.flybywiresim.com/pilots-corner/beginner-guide/takeoff-climb-cruise/)',
+                '- [Descent Planning and Descent](https://docs.flybywiresim.com/pilots-corner/beginner-guide/descent/)',
+                '- [Approach and ILS Landing](https://docs.flybywiresim.com/pilots-corner/beginner-guide/landing/)',
+                '- [After Landing and Taxi to Gate](https://docs.flybywiresim.com/pilots-corner/beginner-guide/after-landing/)',
+                '- [Powering Down](https://docs.flybywiresim.com/pilots-corner/beginner-guide/powering-down/)',
+                '- [Abbreviations](https://docs.flybywiresim.com/pilots-corner/beginner-guide/abbreviations/)',
+            ]),
         });
 
         await msg.channel.send({ embeds: [beginnerEmbed] });
