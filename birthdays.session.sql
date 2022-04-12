fe72b4325a13f26dc9d41b54880776150247e812
Hypeism <jhnwllms236@gmail.com>
2022-04-12 02:45:17 -0400

    sql template updates, TODO: syntax corrections

--- birthdays.session.sql
+++ birthdays.session.sql
@@ -7,28 +7,28 @@ CREATE TABLE birthdays(
     birthday INT,
     discord_id INT NOT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (owner_id) REFERENCES Users(id)
+    FOREIGN KEY (discord_id) REFERENCES Users(id)
 );
 
 -- @BLOCK
-INSERT INTO Rooms (owner_id, street)
+INSERT INTO birthdays (discord_id)
 VALUES
-    (1, 'san diego sailboat'),
-    (1, 'Kyoto townhouse'),
-    (1, 'French Villa'),
-    (1, 'sf cardboard box');
+    (1, '111999'),
+    (1, '111994'),
+    (1, '111995'),
+    (1, '111990');
 
 -- @BLOCK
 SELECT * FROM users
-LEFT JOIN Rooms
-ON Rooms.owner_id = Users.id;
+LEFT JOIN discord_id
+ON users.discord_id = Users.id;
 
 -- @block
 SELECT 
-    Users.id AS user_id,
-    Rooms.id AS room_id,
-    email,
-    street
+    Users.id AS discord_id
+    birthday.id AS birthday_id,
+    discord_id
+    birthday
 
 FROM users
-INNER JOIN Rooms ON Rooms.owner_id = Users.id;
+INNER JOIN birthday ON birthday.id = Users.id;
