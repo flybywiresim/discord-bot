4245f185c1e3ce2d2a0aa8fd30c27f0425ae60cb
Jordan Doose <peregrine.developments@gmail.com>
2022-04-12 01:22:45 -0700

    fix: metar command error handling

--- src/commands/utils/metar.ts
+++ src/commands/utils/metar.ts
@@ -21,31 +21,49 @@ export const metar: CommandDefinition = {
             headers: {
                 Authorization: process.env.METAR_TOKEN },
         }, async (error, response, body) => {
-            const metarReport = JSON.parse(body);
-            const metarEmbed = makeEmbed({
-                title: `METAR Report | ${metarReport.station}`,
-                description: makeLines([
-                    '**Raw Report**',
-                    metarReport.raw,
-                    ,
-                    '**Basic Report:**',
-                    `**Time Observed:** ${metarReport.time.dt}`,
-                    `**Station:** ${metarReport.station}`,
-                    `**Wind:** ${metarReport.wind_direction.repr} at ${metarReport.wind_speed.repr}kts`,
-                    `**Visibility:** ${metarReport.visibility.repr}${metarReport.units.visibility}`,
-                    `**Temperature:** ${metarReport.temperature.repr}C`,
-                    `**Dew Point:** ${metarReport.dewpoint.repr}C`,
-                    `**Altimeter:** ${metarReport.altimeter.value.toString()} ${metarReport.units.altimeter}`,
-                ]),
-                fields: [
-                    {
-                        name: 'Unsure of how to read the raw report?',
-                        value: 'Please refer to our guide [here.](https://docs.flybywiresim.com/pilots-corner/airliner-flying-guide/weather/)',
-                        inline: false
-                    },
-                ],
-                footer: { text: 'This METAR report may not accurately reflect the weather in the simulator. However, it will always be similar to the current conditions present in the sim.' },
-            });
+            let metarEmbed;
+
+            if(response.statusCode == 200) {
+                const metarReport = JSON.parse(body);
+                metarEmbed = makeEmbed({
+                    title: `METAR Report | ${metarReport.station}`,
+                    description: makeLines([
+                        '**Raw Report**',
+                        metarReport.raw,
+                        ,
+                        '**Basic Report:**',
+                        `**Time Observed:** ${metarReport.time.dt}`,
+                        `**Station:** ${metarReport.station}`,
+                        `**Wind:** ${metarReport.wind_direction.repr} at ${metarReport.wind_speed.repr}kts`,
+                        `**Visibility:** ${metarReport.visibility.repr}${metarReport.units.visibility}`,
+                        `**Temperature:** ${metarReport.temperature.repr}C`,
+                        `**Dew Point:** ${metarReport.dewpoint.repr}C`,
+                        `**Altimeter:** ${metarReport.altimeter.value.toString()} ${metarReport.units.altimeter}`,
+                    ]),
+                    fields: [
+                        {
+                            name: 'Unsure of how to read the raw report?',
+                            value: 'Please refer to our guide [here.](https://docs.flybywiresim.com/pilots-corner/airliner-flying-guide/weather/)',
+                            inline: false
+                        },
+                    ],
+                    footer: { text: 'This METAR report may not accurately reflect the weather in the simulator. However, it will always be similar to the current conditions present in the sim.' },
+                });
+            } else if(response.statusCode == 400) {
+                metarEmbed = makeEmbed({
+                    title: `METAR Error | ${icaoArg.toUpperCase()}`,
+                    description: makeLines([
+                        `${icaoArg.toUpperCase()} is not a valid station code!`,
+                    ]),
+                });
+            } else {
+                metarEmbed = makeEmbed({
+                    title: `METAR Error`,
+                    description: makeLines([
+                        `There was an unknown error with the METAR request!`,
+                    ]),
+                });
+            }
 
             await msg.channel.send({ embeds: [metarEmbed] });
 
