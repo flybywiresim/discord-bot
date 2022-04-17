951c42fd188cb9cd5c8346efa7144a4cbdbf6558
Owen <100061049+owen2007@users.noreply.github.com>
2022-04-18 08:21:55 +1000

    adds change requests to the github command

--- src/commands/support/github.ts
+++ src/commands/support/github.ts
@@ -3,16 +3,16 @@ import { CommandCategory } from '../../constants';
 import { makeEmbed, makeLines } from '../../lib/embed';
 
 export const github: CommandDefinition = {
-    name: 'github',
-    description: 'provides the link to the a32nx github page',
+    name: ['github', 'repo', 'repository'],
+    description: 'provides the link to the A32NX GitHub Repository',
     category: CommandCategory.SUPPORT,
     executor: async (msg) => {
         const githubEmbed = makeEmbed({
-            title: 'FlyByWire A32NX | github page',
+            title: 'FlyByWire A32NX | GitHub Repository',
             description: makeLines ([
-                'Below is the link to our a32nx github page',
+                'Below is the link to our A32NX GitHub Repository',
                 '',
-                '[a32nx github page](https://github.com/flybywiresim/a32nx)',
+                '[A32NX GitHub Repository](https://github.com/flybywiresim/a32nx)',
             ]),
         });
 
