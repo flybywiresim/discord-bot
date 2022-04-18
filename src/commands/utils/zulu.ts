78c74e725dc4cd76a257e42355e24886e5a47633
Sam <93292288+Earthsam12@users.noreply.github.com>
2022-04-17 20:50:49 -0400

    remove .z alias and allow no arg (UTC+0)

--- src/commands/utils/zulu.ts
+++ src/commands/utils/zulu.ts
@@ -5,16 +5,13 @@ import { CommandCategory } from '../../constants';
 const dateFormat = 'dddd, DD MMMM LT'; // ? DD MMMM or MMMM DD
 
 export const zulu: CommandDefinition = {
-    name: ['zulu', 'z'],
+    name: 'zulu',
     description: 'Get the current time at a given UTC-offset timezone',
     category: CommandCategory.UTILS,
     executor: async (msg) => {
         const args = msg.content.split(' ').slice(1);
-        if (args.length === 0) {
-            return msg.reply('You need to provide a UTC offset timezone.');
-        }
-        const utcOffset = parseFloat(args.join(''));
+        const utcOffset = args.length ? parseFloat(args.join('')) : 0;
 
-        return msg.channel.send(`It is ${moment().utc().add(utcOffset, 'hours').format(dateFormat)} in that timezone (UTC${utcOffset > 0 ? '+' : ''}${utcOffset})`);
+        return msg.channel.send(`It is ${moment().utc().add(utcOffset, 'hours').format(dateFormat)} in that timezone (UTC${utcOffset >= 0 ? '+' : ''}${utcOffset})`);
     },
 };
