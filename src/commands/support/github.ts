f906fea9375cae39d51379dfedb57d2752476010
Owen <100061049+owen2007@users.noreply.github.com>
2022-04-19 20:24:54 +1000

    updating github command with change requests

--- src/commands/support/github.ts
+++ src/commands/support/github.ts
@@ -9,7 +9,7 @@ export const github: CommandDefinition = {
     executor: async (msg) => {
         const githubEmbed = makeEmbed({
             title: 'FlyByWire A32NX | GitHub Repository',
-            description: makeLines ([
+            description: makeLines([
                 'Below is the link to our A32NX GitHub Repository',
                 '',
                 '[A32NX GitHub Repository](https://github.com/flybywiresim/a32nx)',
