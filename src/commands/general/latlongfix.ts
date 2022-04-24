a80dcd589a2f32376f1e6110d6eed49b413fb14b
Hypeism <jhnwllms236@gmail.com>
2022-04-24 01:47:51 -0400

    added conversion method for north atlantic half-lattitude fixes, message and link for non db waypoints, and a footer to the bot message

--- src/commands/general/latlongfix.ts
+++ src/commands/general/latlongfix.ts
@@ -9,7 +9,9 @@ export const latlongfix: CommandDefinition = {
     executor: async (msg) => {
         const latlongfixEmbed = makeEmbed({
             title: 'FlyByWire | Latitude & Longitude Fix Conversion Chart',
-            image: { url: 'https://cdn.discordapp.com/attachments/817517626045104149/967344118554570772/oceanicfixes.png' },
+            description: 'If the point you\'re looking for isn\'t part of the database, check out our guide on [adding and storing custom waypoints.](https://docs.flybywiresim.com/pilots-corner/advanced-guides/data-management/?h=data+man#format-latitude-longitude-ll)',
+            image: { url: 'https://cdn.discordapp.com/attachments/817517626045104149/967659373054816286/unknown.png' },
+            footer: { text: 'Tip: Click the image to view in full size' }
         });
 
         await msg.channel.send({ embeds: [latlongfixEmbed] });
