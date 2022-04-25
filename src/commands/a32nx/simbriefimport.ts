4b9b669cf1dac0ff07e68c4e17f3c8b69dc88b6c
Ben Williams <bwilliams12@me.com>
2022-04-25 20:23:30 +0100

    Change link

--- src/commands/a32nx/simbriefimport.ts
+++ src/commands/a32nx/simbriefimport.ts
@@ -10,7 +10,7 @@ export const simbriefimport: CommandDefinition = {
         const simbriefImportEmbed = makeEmbed({
             title: 'FlyByWire A32NX | simBrief import',
             description: makeLines([
-                'The A32NX has simBrief integration. This means you can create a flight plan in simBrief and import it into the FMS. Please refer to our [documentation](https://docs.flybywiresim.com/pilots-corner/beginner-guide/preflight/#flight-plan-import) for instructions.',
+                'The A32NX has simBrief integration. This means you can create a flight plan in simBrief and import it into the FMS. Please refer to our [documentation](https://docs.flybywiresim.com/fbw-a32nx/feature-guides/simbrief/) for instructions.',
             ]),
         });
 
