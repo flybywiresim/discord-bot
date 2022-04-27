76b3daa2361bd5dbe85e6894bd77b66b9be0ec12
Hypeism <jhnwllms236@gmail.com>
2022-04-26 21:23:31 -0400

    added hint for errors related to FL inputs

--- src/commands/utils/tod.ts
+++ src/commands/utils/tod.ts
@@ -10,14 +10,19 @@ export const tod: CommandDefinition = {
         const text = msg.content.split(' ').slice(1).join('');
         const altitude = parseInt(text.replace(/[^0-9.]/g, ''), 10);
         const error = 'Please specify a valid altitude or flight level.';
+        const flightLevelError = 'Hint: Try appending \'FL\' to the beginning of values below 1000';
         const errorEmbed = makeEmbed({
             title: 'Error',
             description: error,
         });
+        const flightLevelerrorEmbed = makeEmbed({
+            title: 'Error',
+            description: flightLevelError,
+        });
         if (Number.isNaN(altitude)) {
             await msg.channel.send({ embeds: [errorEmbed] });
         } if (!msg.content.includes('FL') && altitude <= 1000) {
-            await msg.channel.send({ embeds: [errorEmbed] });
+            await msg.channel.send({ embeds: [flightLevelerrorEmbed] });
         } if (altitude <= 1000 && text.startsWith('FL')) {
             const todFL = Math.floor(altitude * (1 / 3) * (11 / 10));
             const todFlightLevelEmbed = makeEmbed({
