2dff35f10919c8f4320d5ca31b62a797205d19f5
Hypeism <jhnwllms236@gmail.com>
2022-04-26 21:29:05 -0400

    added spaces for readability

--- src/commands/utils/tod.ts
+++ src/commands/utils/tod.ts
@@ -8,34 +8,50 @@ export const tod: CommandDefinition = {
     category: CommandCategory.UTILS,
     executor: async (msg) => {
         const text = msg.content.split(' ').slice(1).join('');
+
         const altitude = parseInt(text.replace(/[^0-9.]/g, ''), 10);
+
         const error = 'Please specify a valid altitude or flight level.';
+
         const flightLevelError = 'Hint: Try appending \'FL\' to the beginning of values below 1000';
+
         const errorEmbed = makeEmbed({
             title: 'Error',
             description: error,
         });
+
         const flightLevelerrorEmbed = makeEmbed({
             title: 'Error',
             description: flightLevelError,
         });
+
         if (Number.isNaN(altitude)) {
             await msg.channel.send({ embeds: [errorEmbed] });
+
         } if (!msg.content.includes('FL') && altitude <= 1000) {
             await msg.channel.send({ embeds: [flightLevelerrorEmbed] });
+
         } if (altitude <= 1000 && text.startsWith('FL')) {
+
             const todFL = Math.floor(altitude * (1 / 3) * (11 / 10));
+
             const todFlightLevelEmbed = makeEmbed({
+
                 title: 'FlyByWire | Descent Approximation',
                 description: `Begin your descent at about ${todFL} nm from touchdown.`,
             });
+
             await msg.channel.send({ embeds: [todFlightLevelEmbed] });
+
         } if (altitude >= 1000) {
+
             const topOfDescent = Math.floor(altitude * (1 / 100) * (1 / 3) * (11 / 10));
+
             const todEmbed = makeEmbed({
                 title: 'FlyByWire | Descent Approximation',
                 description: `Begin your descent at about ${topOfDescent} nm from touchdown.`,
             });
+
             await msg.channel.send({ embeds: [todEmbed] });
         }
     },
