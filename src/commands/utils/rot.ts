b8ca6ca90b9be275c15b33ffe05cd0e3d96afb5c
NakajimaYoshi <52870481+NakajimaYoshi@users.noreply.github.com>
2022-05-07 18:30:54 -0400

    removed extranous whitespace

--- src/commands/utils/rot.ts
+++ src/commands/utils/rot.ts
@@ -8,23 +8,17 @@ export const rot: CommandDefinition = {
     category: CommandCategory.UTILS,
     executor: async (msg) => {
         const text = msg.content.split(' ').slice(1).join('');
-
         const altitude = parseInt(text.replace(/[^0-9.]/g, ''), 10);
-
         const error = 'Please specify a valid altitude or flight level.';
-
         const flightLevelError = 'Hint: Try appending \'FL\' to the beginning of values below 1000';
-
         const errorEmbed = makeEmbed({
             title: 'Error',
             description: error,
         });
-
         const flightLevelerrorEmbed = makeEmbed({
             title: 'Error',
             description: flightLevelError,
         });
-
         if (Number.isNaN(altitude)) {
             await msg.channel.send({ embeds: [errorEmbed] });
         } if (!msg.content.includes('FL') && altitude <= 1000) {
@@ -44,7 +38,6 @@ export const rot: CommandDefinition = {
         } if (altitude >= 1000) {
             const topOfDescent = Math.floor(altitude * (1 / 100) * (1 / 3) * (11 / 10));
             const todEmbed = makeEmbed({
-
                 title: 'FlyByWire | Descent Approximation',
                 description: makeLines([
                     `To descend a total of **${altitude}ft**, start a -3.0° descent **${topOfDescent}nm** from the point you wish to reach the target altitude.`,
@@ -53,7 +46,6 @@ export const rot: CommandDefinition = {
                     + 'See the official [guide](https://docs.flybywiresim.com/pilots-corner/beginner-guide/descent/#how-to-calculate-the-required-distance-for-descent) for planning an optimal descent.',
                 ]),
             });
-
             await msg.channel.send({ embeds: [todEmbed] });
         }
     },
