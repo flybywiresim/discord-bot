1f78ffe4b755c092fe5490143728e36b364dae34
Valastiri <reyes.nico@gmail.com>
2022-05-09 19:53:46 +0800

    sabes review changes

--- src/commands/a32nx/recommendedsettings.ts
+++ src/commands/a32nx/recommendedsettings.ts
@@ -12,8 +12,7 @@ export const recommendedSettings: CommandDefinition = {
             description: makeLines([
                 'Before embarking on your flight please ensure you have looked at our recommended settings guide.',
                 '',
-                'This page aims to list a few recommended settings for the Microsoft Flight Simulator, '
-                + 'Windows and the A32NX itself.',
+                'This page aims to list a few recommended settings for Microsoft Flight Simulator, Windows and the A32NX.',
                 '',
                 'Please read our guide [here.](https://docs.flybywiresim.com/fbw-a32nx/settings/)',
             ]),
