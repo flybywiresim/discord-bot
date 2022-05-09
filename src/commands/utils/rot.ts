5394f090e351996d365f38e8482ddf148b49b5ec
Nathan Innes <nathaninnes001@gmail.com>
2022-05-09 11:02:25 +0100

    lint: improve readability

--- src/commands/utils/rot.ts
+++ src/commands/utils/rot.ts
@@ -11,6 +11,7 @@ export const rot: CommandDefinition = {
         const altitude = parseInt(text.replace(/[^0-9.]/g, ''), 10);
         const error = 'Please specify a valid altitude or flight level.';
         const flightLevelError = 'Hint: Try appending \'FL\' to the beginning of values below 1000';
+
         const errorEmbed = makeEmbed({
             title: 'Error',
             description: error,
@@ -19,31 +20,35 @@ export const rot: CommandDefinition = {
             title: 'Error',
             description: flightLevelError,
         });
+
         if (Number.isNaN(altitude)) {
             await msg.channel.send({ embeds: [errorEmbed] });
-        } if (!msg.content.includes('FL') && altitude <= 1000) {
+        }
+        if (!msg.content.includes('FL') && altitude <= 1000) {
             await msg.channel.send({ embeds: [flightLevelerrorEmbed] });
-        } if (altitude <= 1000 && text.startsWith('FL')) {
+        }
+
+        if (altitude <= 1000 && text.startsWith('FL')) {
             const todFL = Math.floor(altitude * (1 / 3) * (11 / 10));
             const todFlightLevelEmbed = makeEmbed({
                 title: 'FlyByWire | Descent Approximation',
                 description: makeLines([
                     `To descend a total of **${altitude}00ft**, start a -3.0° descent **${todFL}nm** from the point you wish to reach the target altitude.`,
                     '',
-                    'This is an approximation of your TOD using the rule of three. '
-                    + 'See the official [guide](https://docs.flybywiresim.com/pilots-corner/beginner-guide/descent/#how-to-calculate-the-required-distance-for-descent) for planning an optimal descent.',
+                    'This is an approximation of your TOD using the rule of three. See the official [guide](https://docs.flybywiresim.com/pilots-corner/beginner-guide/descent/#how-to-calculate-the-required-distance-for-descent) for planning an optimal descent.',
                 ]),
             });
             await msg.channel.send({ embeds: [todFlightLevelEmbed] });
-        } if (altitude >= 1000) {
+        }
+
+        if (altitude >= 1000) {
             const topOfDescent = Math.floor(altitude * (1 / 100) * (1 / 3) * (11 / 10));
             const todEmbed = makeEmbed({
                 title: 'FlyByWire | Descent Approximation',
                 description: makeLines([
                     `To descend a total of **${altitude}ft**, start a -3.0° descent **${topOfDescent}nm** from the point you wish to reach the target altitude.`,
                     '',
-                    'This is an approximation of your TOD using the rule of three. '
-                    + 'See the official [guide](https://docs.flybywiresim.com/pilots-corner/beginner-guide/descent/#how-to-calculate-the-required-distance-for-descent) for planning an optimal descent.',
+                    'This is an approximation of your TOD using the rule of three. See the official [guide](https://docs.flybywiresim.com/pilots-corner/beginner-guide/descent/#how-to-calculate-the-required-distance-for-descent) for planning an optimal descent.',
                 ]),
             });
             await msg.channel.send({ embeds: [todEmbed] });
