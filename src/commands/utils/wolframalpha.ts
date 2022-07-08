1b7316dd11d4f1f6d518e63d572a876c5d0ac246
Nathan Innes <nathaninnes001@gmail.com>
2022-07-08 04:56:00 +0100

    refactor:  wolframalpha request

--- src/commands/utils/wolframalpha.ts
+++ src/commands/utils/wolframalpha.ts
@@ -1,11 +1,11 @@
 import { URLSearchParams } from 'url';
-import request from 'request';
+import fetch from 'node-fetch';
 import { CommandDefinition } from '../../lib/command';
 import { makeEmbed, makeLines } from '../../lib/embed';
-import { CommandCategory, Channels } from '../../constants';
+import { CommandCategory } from '../../constants';
 import Logger from '../../lib/logger';
 
-const WOFLRAMALPHA_API_URL = 'http://api.wolframalpha.com/v2/query?';
+const WOLFRAMALPHA_API_URL = 'http://api.wolframalpha.com/v2/query?';
 const WOLFRAMALPHA_QUERY_URL = 'http://www.wolframalpha.com/input/?';
 
 export const wolframalpha: CommandDefinition = {
@@ -13,70 +13,68 @@ export const wolframalpha: CommandDefinition = {
     description: 'Queries the Wolfram Alpha API',
     category: CommandCategory.UTILS,
     executor: async (msg) => {
-        const allowedChannel = Channels.BOT_COMMANDS;
-        if (msg.channelId === allowedChannel) {
-            const re = /\.\S+\s(.+)/;
-            let query: string = '';
-            try {
-                // eslint-disable-next-line prefer-destructuring
-                query = msg.content.match(re)[1];
-            } catch (e) {
-                Logger.error(e);
-            }
-            if (query.length <= 0) {
-                await msg.reply('Please enter a query.');
+        const splitUp = msg.content.replace(/\.wa\s+/, ' ').split(' ');
+
+        if (splitUp.length <= 1) {
+            await msg.reply('Please provide a query. For example: .wa "2 + 2"');
+            return;
+        }
+        const query = splitUp.slice(1).join(' ');
+
+        const params = {
+            appid: process.env.WOLFRAMALPHA_TOKEN,
+            input: query,
+            format: 'plaintext',
+            output: 'JSON',
+        };
+
+        const searchParams = new URLSearchParams(params);
+
+        try {
+            const response = await fetch(`${WOLFRAMALPHA_API_URL}${searchParams.toString()}`)
+                .then((res) => res.json());
+
+            if (response.error) {
+                await msg.reply(response.error);
                 return;
             }
-            const params = {
-                appid: process.env.WOLFRAMALPHA_TOKEN,
-                input: query,
-                format: 'plaintext',
-                output: 'JSON',
-            };
-            const searchParams = new URLSearchParams(params);
-            request({
-                method: 'GET',
-                url: WOFLRAMALPHA_API_URL + searchParams.toString(),
-            }, async (error, response, body) => {
-                if (response.statusCode === 200) {
-                    const answer = JSON.parse(body);
-                    if (answer.queryresult.success === true) {
-                        const podTexts: string[] = [];
-                        answer.queryresult.pods.forEach((pod) => {
-                            if (pod.id !== 'Input' && pod.primary === true) {
-                                const results: string[] = [];
-                                pod.subpods.forEach((subpod) => {
-                                    results.push(subpod.plaintext);
-                                });
-                                if (results.length > 0) {
-                                    podTexts.push(`**${pod.title}:** \n${results.join('\n')}`);
-                                }
-                            }
+
+            if (response.queryresult.success === true) {
+                const podTexts: string[] = [];
+                response.queryresult.pods.forEach((pod) => {
+                    if (pod.id !== 'Input' && pod.primary === true) {
+                        const results: string[] = [];
+                        pod.subpods.forEach((subpod) => {
+                            results.push(subpod.plaintext);
                         });
-                        if (podTexts.length > 0) {
-                            const result = podTexts.join('\n\n');
-                            const queryParams = new URLSearchParams({ i: query });
-                            // await msg.channel.send(result + ' - [Web Result](' + WOLFRAMALPHA_QUERY_URL + queryParams.toString() + ')');
-
-                            const waEmbed = makeEmbed({
-                                description: makeLines([
-                                    result,
-                                    '',
-                                    `[Web Result](${WOLFRAMALPHA_QUERY_URL}${queryParams.toString()})`,
-                                ]),
-                            });
-
-                            await msg.reply({ embeds: [waEmbed] });
-                        } else {
-                            await msg.reply('Wolfram Alpha did not give an answer.');
+                        if (results.length > 0) {
+                            podTexts.push(`**${pod.title}:** \n${results.join('\n')}`);
                         }
-                    } else {
-                        await msg.reply('Wolfram Alpha could not understand your query.');
                     }
+                });
+                if (podTexts.length > 0) {
+                    const result = podTexts.join('\n\n');
+                    const queryParams = new URLSearchParams({ i: query });
+
+                    const waEmbed = makeEmbed({
+                        description: makeLines([
+                            result,
+                            '',
+                            `[Web Result](${WOLFRAMALPHA_QUERY_URL}${queryParams.toString()})`,
+                        ]),
+                    });
+
+                    await msg.reply({ embeds: [waEmbed] });
+                    return;
                 }
-            });
-        } else {
-            await msg.reply(`Please ask in <#${Channels.BOT_COMMANDS}>`);
+                await msg.reply('Wolfram Alpha did not find an answer for your query.');
+                return;
+            }
+            await msg.reply('Wolfram Alpha could not understand your query.');
+            return;
+        } catch (e) {
+            Logger.error('wolframalpha:', e);
+            await msg.reply('An error occurred while fetching the Wolfram Alpha report.');
         }
     },
 };
