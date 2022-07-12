b22a84f8e6f384b486c865cb0ef399f1c15c3233
Valastiri <reyes.nico@gmail.com>
2022-07-12 16:39:50 +0800

    copy change

--- src/commands/a32nx/weather.ts
+++ src/commands/a32nx/weather.ts
@@ -10,7 +10,7 @@ export const weather: CommandDefinition = {
         const weatherEmbed = makeEmbed({
             title: 'FlyByWire A32NX | Weather Radar + Terrain Display',
             description: makeLines([
-                'All versions of the A32NX do not have operating weather radar and terrain display. This is due to performance issues related to the default systems and the ND being rewritten.',
+                'All versions of the A32NX do not have an operating weather radar and terrain display. This is due to performance issues related to the default systems and the ND being rewritten.',
                 '',
                 'The team are currently waiting on a weather and terrain API to be implemented in order to make a radar that is as realistic as possible. You can read the MSFS forum [here.](https://forums.flightsimulator.com/t/implement-weather-and-terrain-api-s-for-aircraft-developers-to-implement-accurate-radar-predictive-windshear-egpws-and-metar-wind-uplink/442016)',
             ]),
