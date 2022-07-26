13f431960cbaff9e54bf29e98dfe32eeb3a07a91
Imenes <kaporr@gmail.com>
2022-07-26 17:52:35 +0200

    refactor: add steps and relevant image

--- src/commands/support/utf-8.ts
+++ src/commands/support/utf-8.ts
@@ -2,7 +2,7 @@ import { CommandDefinition } from '../../lib/command';
 import { CommandCategory } from '../../constants';
 import { makeEmbed, makeLines } from '../../lib/embed';
 
-const UTF8_HELP_URL = 'https://cdn.discordapp.com/attachments/902990139670814750/1001138306257518682/utf8.PNG';
+const UTF8_HELP_URL = 'https://docs.flybywiresim.com/fbw-a32nx/assets/settings/utf8-windows-setting.png';
 
 export const utf8: CommandDefinition = {
     name: 'utf8',
@@ -12,9 +12,19 @@ export const utf8: CommandDefinition = {
         const utf8Embed = makeEmbed({
             title: 'FlyByWire A32NX | UTF-8',
             description: makeLines([
-                'In rare cases the Autopilot, FADEC and electrical systems may not start or behave erratically. This is in part due to UTF-8 language support beta not enabled on your machine.',
+                'Some users experience problems with various system in the A32NX. These are caused by an issue within MSFS which requires the use of the UTF8 Region setting in Windows.',
                 '',
-                'Please see our [documentation](https://docs.flybywiresim.com/fbw-a32nx/support/reported-issues/?h=utf8#autopilot-fadec-electrical-systems-not-working-as-intended-utf8-issue) for solutions.',
+                'To enable UTF-8 support follow the steps below:',
+                '',
+                '• Open Windows Control Panel -> Region.',
+                '',
+                '• Go to the Administrative tab and click Change system locale.',
+                '',
+                '• Make sure the check mark next to Beta: Use UTF-8 for worldwide language support is selected.',
+                '',
+                '• Click OK and restart your computer.',
+                '',
+                'Please see our [documentation](https://docs.flybywiresim.com/fbw-a32nx/support/reported-issues/?h=utf8#autopilot-fadec-electrical-systems-not-working-as-intended-utf8-issue) for more information.',
             ]),
             image: { url: UTF8_HELP_URL },
         });
