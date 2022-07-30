2dc614bd103ed3446cf193898a64257f55ad730a
Nathan Innes <nathaninnes001@gmail.com>
2022-07-30 05:31:26 +0100

    fix: update to v14

--- src/commands/utils/holidays.ts
+++ src/commands/utils/holidays.ts
@@ -1,4 +1,5 @@
 import fetch from 'node-fetch';
+import { Colors, EmbedField } from 'discord.js';
 import { CommandDefinition } from '../../lib/command';
 import { makeEmbed } from '../../lib/embed';
 import { CommandCategory } from '../../constants';
@@ -10,37 +11,47 @@ export const holidays: CommandDefinition = {
     executor: async (msg) => {
         try {
             const holidays = await fetch('https://date.nager.at/api/v3/NextPublicHolidaysWorldwide').then((res) => res.json());
+            const fields: EmbedField[] = [];
+
+            await holidays.slice(0, 5).map((holiday) => {
+                fields.push({
+                    name: 'Holiday Name',
+                    value: holiday.name,
+                    inline: false,
+                });
+
+                fields.push({
+                    name: 'Date',
+                    value: holiday.date,
+                    inline: true,
+                });
+
+                fields.push({
+                    name: 'Local Name',
+                    value: holiday.localName,
+                    inline: true,
+                });
+
+                fields.push({
+                    name: 'Country Code',
+                    value: holiday.countryCode,
+                    inline: true,
+                });
+                return fields;
+            });
+
             const holidaysEmbed = makeEmbed({
                 title: 'Upcoming Worldwide Holidays',
                 description: 'List of worldwide holidays:',
-                fields: holidays.slice(0, 5).map((holiday) => [
-                    {
-                        name: 'Holiday Name',
-                        value: holiday.name,
-                    },
-                    {
-                        name: 'Date',
-                        value: holiday.date,
-                        inline: true,
-                    },
-                    {
-                        name: 'Local Name',
-                        value: holiday.localName,
-                        inline: true,
-                    },
-                    {
-                        name: 'Country Code',
-                        value: holiday.countryCode,
-                        inline: true,
-                    },
-                ]),
+                fields,
             });
+
             return msg.channel.send({ embeds: [holidaysEmbed] });
         } catch (err) {
             const errorEmbed = makeEmbed({
                 title: 'Error | Public Holidays',
                 description: `An error occurred ${err}`,
-                color: 'RED',
+                color: Colors.Red,
             });
             return msg.reply({ embeds: [errorEmbed] });
         }
