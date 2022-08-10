cc484b1379af0f0122eb26e1171b22af03dde607
Philippe Dellaert <philippe@dellaert.org>
2022-08-09 18:31:24 -0700

    Refactor and add support for multi-string search

--- src/commands/general/doc.ts
+++ src/commands/general/doc.ts
@@ -9,39 +9,45 @@ export const doc: CommandDefinition = {
     description: 'Provides a link to the documentation or documentation search for a quick link if there is no dedicated command.',
     category: CommandCategory.GENERAL,
     executor: (msg) => {
-        const splitUp = msg.content.replace(/\.(doc|documentation|docsearch)\s+/, ' ').split(' ');
+        const searchWords = msg.content.replace(/^\.(doc|documentation|docsearch)\s+/, ' ').split(' ');
 
-        if (splitUp.length <= 1) {
+        if (searchWords.length <= 1) {
             const noQueryEmbed = makeEmbed({
                 title: 'FlyByWire Documentation',
-                description: 'Find the full FlyByWire Documentation [here.](https://docs.flybywiresim.com/)',
+                description: 'Find the full FlyByWire Documentation [here](https://docs.flybywiresim.com/).',
             });
             return msg.channel.send({ embeds: [noQueryEmbed] });
         }
 
-        const searchText = splitUp[1];
-
+        console.log(searchWords);
         // Safety to prevent users from sending their own links in bot output.
-        try {
-            const _ = new URL(searchText);
-            const URLEmbed = makeEmbed({
-                title: 'FlyByWire Documentation | Error',
-                description: 'Providing URLs to the Documentation search command is not allowed.',
-                color: Colors.Red,
-            });
-            return msg.channel.send({ embeds: [URLEmbed] });
-        } catch (_) { /**/ }
+        for (const searchWord of searchWords) {
+            try {
+                const _ = new URL(searchWord);
+                const URLEmbed = makeEmbed({
+                    title: 'FlyByWire Documentation | Error',
+                    description: 'Providing URLs to the Documentation search command is not allowed.',
+                    color: Colors.Red,
+                });
+                return msg.channel.send({ embeds: [URLEmbed] });
+            } catch (_) { /**/ }
+
+            const filter = new Filter();
+            if (filter.isProfane(searchWord)) {
+                return msg.reply('Please do not use profane language with this command.');
+            }
+        }
+
+        // Removing first element as it is an empty string
+        searchWords.shift();
+        const searchQuery = searchWords.join(' ');
 
         // Safety to prevent users from entering unexpected data that might result in strange behavior in a URL.
-        const encodedSearchText = encodeURIComponent(searchText);
-        const filter = new Filter();
-        if (filter.isProfane(searchText)) {
-            return msg.reply('Please do not use profane language with this command.');
-        }
+        const encodedSearchQuery = encodeURIComponent(searchQuery);
 
         const queryEmbed = makeEmbed({
             title: 'FlyByWire Documentation Search',
-            description: `Find the FlyByWire Documentation for "${searchText}" [here.](https://docs.flybywiresim.com/?q=${encodedSearchText})`,
+            description: `Search the FlyByWire Documentation for "${searchQuery}" [here](https://docs.flybywiresim.com/?q=${encodedSearchQuery}).`,
         });
         return msg.channel.send({ embeds: [queryEmbed] });
     },
