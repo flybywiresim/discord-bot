ef290470bb5db71bb89c80a485791497ed04f622
Nathan Innes <nathaninnes001@gmail.com>
2022-08-15 23:59:45 +0100

    Revert "feat: msfs alias"
    This reverts commit 10c8f8c679d5796b36cbbf0c106f11907079daca.

--- src/commands/general/msfsdiscord.ts
+++ src/commands/general/msfsdiscord.ts
@@ -4,7 +4,7 @@ import { CommandCategory } from '../../constants';
 const MSFS_DISCORD_LINK = 'https://discord.gg/msfs';
 
 export const msfsdisc: CommandDefinition = {
-    name: ['msfs', 'msfsdisc', 'fsdisc', 'msfsdiscord'],
+    name: ['msfsdisc', 'fsdisc', 'msfsdiscord'],
     description: 'Provides link to Microsoft Flight Simulator discord server',
     category: CommandCategory.GENERAL,
     executor: (msg) => msg.channel.send(MSFS_DISCORD_LINK),
