d4caefe3bfb6f27016b84ee93c16b85c16e12304
Nathan Innes <nathaninnes001@gmail.com>
2022-08-16 00:06:23 +0100

    refactor: remove async/await from invites

--- src/commands/general/headwind.ts
+++ src/commands/general/headwind.ts
@@ -5,7 +5,5 @@ export const headwind: CommandDefinition = {
     name: ['headwind', 'hw'],
     description: 'Provides link to the Headwind Discord server',
     category: CommandCategory.GENERAL,
-    executor: async (msg) => {
-        await msg.channel.send('https://discord.gg/SYanvKEX7B');
-    },
+    executor: (msg) => msg.channel.send('https://discord.gg/SYanvKEX7B'),
 };
