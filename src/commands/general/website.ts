687d9f6fc6813105dd50e18c4e863dc6e0f2aa82
Nathan Innes <nathaninnes001@gmail.com>
2022-08-22 06:09:15 +0100

    refactor: rename const

--- src/commands/general/website.ts
+++ src/commands/general/website.ts
@@ -1,11 +1,11 @@
 import { CommandDefinition } from '../../lib/command';
 import { CommandCategory } from '../../constants';
 
-const WEB_URL = 'https://flybywiresim.com/';
+const WEBSITE_URL = 'https://flybywiresim.com/';
 
 export const website: CommandDefinition = {
     name: ['website', 'web'],
     description: 'Links to the FlyByWire Simulations website.',
     category: CommandCategory.GENERAL,
-    executor: (msg) => msg.channel.send(WEB_URL),
+    executor: (msg) => msg.channel.send(WEBSITE_URL),
 };
