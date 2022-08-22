0f36ac0bf467cd64db5b3c30851c51c98ced905b
Nathan Innes <nathaninnes001@gmail.com>
2022-08-22 14:00:37 +0100

    feat: website command (#341)
    * feat: website command
    
    * fix: missing command category
    
    * refactor: rename const

---
+++ src/commands/general/website.ts
@@ -0,0 +1,11 @@
+import { CommandDefinition } from '../../lib/command';
+import { CommandCategory } from '../../constants';
+
+const WEBSITE_URL = 'https://flybywiresim.com/';
+
+export const website: CommandDefinition = {
+    name: ['website', 'web'],
+    description: 'Links to the FlyByWire Simulations website.',
+    category: CommandCategory.GENERAL,
+    executor: (msg) => msg.channel.send(WEBSITE_URL),
+};
