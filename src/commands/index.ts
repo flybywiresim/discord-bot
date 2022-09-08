42045eabab9e203e2924862f9c6f399849258d56
Nufflee <zifcani@gmail.com>
2022-09-08 02:45:56 +0200

    feat: add .dfd command

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -143,6 +143,7 @@ import { verticalGuidanceSymbols } from './a32nx/verticalGuidanceSymbols';
 import { verticalSpeedPriority } from './a32nx/verticalSpeedPriority';
 import { flyPadOS } from './a32nx/flyPadOS';
 import { simulationRate } from './support/simulationRate';
+import { dfd } from './general/dfd';
 
 const commands: CommandDefinition[] = [
     ping,
@@ -288,6 +289,7 @@ const commands: CommandDefinition[] = [
     verticalSpeedPriority,
     flyPadOS,
     simulationRate,
+    dfd,
 ];
 
 const commandsObject: { [k: string]: CommandDefinition } = {};
