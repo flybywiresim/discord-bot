db1da5572bff85e79b3625d8395043cf8d2a0666
Philippe Dellaert <philippe@dellaert.org>
2022-11-07 18:27:37 -0800

    Improving language

--- src/commands/support/wasm.ts
+++ src/commands/support/wasm.ts
@@ -9,7 +9,7 @@ export const wasm: CommandDefinition = {
     executor: (msg) => {
         const wasmEmbed = makeEmbed({
             title: 'FlyByWire Support | Long loading times',
-            description: 'The first load after installing or updating the addon will take a while due to WASM being compiled. This is normal and may take more than 15 minutes or even longer on slower PCs.',
+            description: 'The first load after installing or updating the addon will take a while due to WASM being compiled. This is normal and may take over 15 minutes on slower PCs.',
         });
 
         return msg.channel.send({ embeds: [wasmEmbed] });
