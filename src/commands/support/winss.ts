adb38727363e7ca0f90cad7dfbed28fc45124c9e
AseCoder <51131379+AseCoder@users.noreply.github.com>
2022-11-11 17:40:32 +0200

    required fixes
    add to changelog and command docs, add image to assets, fix tab indent

--- src/commands/support/winss.ts
+++ src/commands/support/winss.ts
@@ -2,26 +2,26 @@ import { CommandDefinition } from '../../lib/command';
 import { makeEmbed, makeLines } from '../../lib/embed';
 import { CommandCategory } from '../../constants';
 
-const SNIPPING_TOOL_URL = 'https://www.howtogeek.com/wp-content/uploads/2016/11/wst_1-1.png';
+const SNIPPING_TOOL_URL = 'https://media.discordapp.net/attachments/897491699167793182/1040649316672479232/image.png';
 
 export const winss: CommandDefinition = {
-	name: ['winss', 'sswin'],
-	description: 'How to screenshot on Windows',
-	category: CommandCategory.SUPPORT,
-	executor: (msg) => {
-		const winssEmbed = makeEmbed({
-			title: 'How to screenshot on Windows',
-			description: makeLines([
-				'1. Press `Win + Shift + S` to start taking a screenshot.',
-				'2. Press and drag to make a rectangle selection of everything you want to include in the screenshot.',
-				'3. Release the mouse to finish the selection.',
-				'4. The screenshot is saved into your clipboard. Paste it in chat with\n`Ctrl+V`.',
-				'',
-				'Alternatively, you can use the Snipping Tool. Click \'New\' and follow\nsteps 2-4.'
-			]),
-			image: { url: SNIPPING_TOOL_URL },
-		});
+    name: ['winss', 'sswin'],
+    description: 'Explains how to screenshot on Windows',
+    category: CommandCategory.SUPPORT,
+    executor: (msg) => {
+        const winssEmbed = makeEmbed({
+            title: 'How to screenshot on Windows',
+            description: makeLines([
+                '1. Press `Win + Shift + S` to start taking a screenshot.',
+                '2. Press and drag to make a rectangle selection of everything you want to include in the screenshot.',
+                '3. Release the mouse to finish the selection.',
+                '4. The screenshot is saved into your clipboard. Paste it in chat with `Ctrl + V`.',
+                '',
+                'Alternatively, you can use the Snipping Tool. Click \'New\' and follow steps 2 - 4.'
+            ]),
+            image: { url: SNIPPING_TOOL_URL },
+        });
 
-		return msg.channel.send({ embeds: [winssEmbed] });
-	},
+        return msg.channel.send({ embeds: [winssEmbed] });
+    },
 };
