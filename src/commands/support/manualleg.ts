96954b32e36df9cd027076fca259e8a427de819d
Philippe Dellaert <philippe@dellaert.org>
2022-12-11 20:08:14 -0800

    fix: manualleg image of the doc site and add alias

--- src/commands/support/manualleg.ts
+++ src/commands/support/manualleg.ts
@@ -1,21 +1,19 @@
-import { CommandDefinition } from '../../lib/command';
+import { MessageCommandDefinition } from '../../lib/command';
 import { makeEmbed } from '../../lib/embed';
 import { CommandCategory } from '../../constants';
 
-const MANUAL_LEG_IMAGE_URL = 'https://cdn.discordapp.com/attachments/752801628347957248/929071827752480839/mcdu-discontinuity-manual-conceptual.png';
+const MANUAL_LEG_IMAGE_URL = 'https://docs.flybywiresim.com/pilots-corner/assets/advanced-guides/disco/mcdu-discontinuity-manual-conceptual.png';
 const DISCON_DOCS_URL = 'https://docs.flybywiresim.com/pilots-corner/advanced-guides/flight-planning/disco/';
 
-export const manualleg: CommandDefinition = {
-    name: ['manualleg', 'vm'],
+const genericManualLegEmbed = makeEmbed({
+    title: 'FlyByWire Support | Manual Leg',
+    image: { url: MANUAL_LEG_IMAGE_URL },
+    description: `Please see our [documentation](${DISCON_DOCS_URL}) for information on how to deal with a manual leg in your flight plan.`,
+});
+
+export const manualleg: MessageCommandDefinition = {
+    name: ['manualleg', 'manual'],
     description: 'Displays image and links to docs about manual waypoints',
     category: CommandCategory.SUPPORT,
-    executor: (msg) => {
-        const manualLegEmbed = makeEmbed({
-            title: 'FlyByWire Support | Manual Leg',
-            image: { url: MANUAL_LEG_IMAGE_URL },
-            description: `Please see our [documentation](${DISCON_DOCS_URL}) for information on how to deal with a manual leg in your flight plan.`,
-        });
-
-        return msg.channel.send({ embeds: [manualLegEmbed] });
-    },
+    genericEmbed: genericManualLegEmbed,
 };
