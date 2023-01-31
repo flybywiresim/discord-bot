58abb9960191c5b753879a751036a0e746b9ca14
ExampleWasTaken <58574351+ExampleWasTaken@users.noreply.github.com>
2023-01-31 20:34:28 +0100

    fix: lint

--- src/commands/support/econnreset.ts
+++ src/commands/support/econnreset.ts
@@ -1,6 +1,6 @@
-import { MessageCommandDefinition } from "../../lib/command";
-import { CommandCategory } from "../../constants";
-import { makeEmbed, makeLines } from "../../lib/embed";
+import { MessageCommandDefinition } from '../../lib/command';
+import { CommandCategory } from '../../constants';
+import { makeEmbed, makeLines } from '../../lib/embed';
 
 export const econnreset: MessageCommandDefinition = {
     name: ['econnreset', 'econreset'],
@@ -10,8 +10,8 @@ export const econnreset: MessageCommandDefinition = {
         description: makeLines([
             'To verify that this issue is not caused by your ISP, please use a VPN set to the US and try again.',
             '',
-            'We recommend [ProtonVPN](https://protonvpn.com/free-vpn/)* as they offer a free plan.'
+            'We recommend [ProtonVPN](https://protonvpn.com/free-vpn/)* as they offer a free plan.',
         ]),
-        footer: { text: '*ProtonVPN is not associated with FlyByWire Simulations. We only suggest this solution as it has been proven to work in the past.' }
+        footer: { text: '*ProtonVPN is not associated with FlyByWire Simulations. We only suggest this solution as it has been proven to work in the past.' },
     }),
-}
+};
