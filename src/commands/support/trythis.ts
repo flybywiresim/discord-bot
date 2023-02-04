194a7c3b1a229fa0f646988211cfbb6703f09de2
ExampleWasTaken <58574351+ExampleWasTaken@users.noreply.github.com>
2023-02-04 20:34:23 +0100

    add link to docs to .trythis cmd

--- src/commands/support/trythis.ts
+++ src/commands/support/trythis.ts
@@ -1,6 +1,12 @@
 import { CommandDefinition } from '../../lib/command';
 import { CommandCategory } from '../../constants';
-import { makeEmbed } from '../../lib/embed';
+import { makeEmbed, makeLines } from '../../lib/embed';
+
+const trythisDescription = makeLines([
+    'Please try and remove all other mods/liveries from the community folder and test our addon again. This will help rule out mod conflicts.',
+    '',
+    'See [this article](https://docs.flybywiresim.com/fbw-a32nx/support/reported-issues/#test-with-only-the-a32nx-add-on-in-community) on how to this quickly.',
+]);
 
 export const trythis: CommandDefinition = {
     name: 'trythis',
@@ -9,7 +15,7 @@ export const trythis: CommandDefinition = {
     executor: (msg) => {
         const trythisEmbed = makeEmbed({
             title: 'Try This',
-            description: 'Please try and remove all other mods/liveries from the community folder and test our addon again. This will help rule out mod conflicts.',
+            description: trythisDescription,
             footer: { text: 'Report back the result of this test.' },
         });
 
