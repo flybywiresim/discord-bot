de1a4b62ba2b3cfb080007e7d01a4130031453c8
AquazDev <71980142+AquazDev@users.noreply.github.com>
2023-02-08 03:22:45 +0200

    Update marshaller.ts

--- src/commands/memes/marshaller.ts
+++ src/commands/memes/marshaller.ts
@@ -2,14 +2,14 @@ import { CommandDefinition } from '../../lib/command';
 import { makeEmbed } from '../../lib/embed';
 import { CommandCategory } from '../../constants';
 
-const ADDON_URL = 'https://tenor.com/view/cute-girl-trying-to-warning-pilot-anime-meme-gif-21114064';
+const MARSHALLER_URL = 'https://tenor.com/view/cute-girl-trying-to-warning-pilot-anime-meme-gif-21114064';
 
 export const addon: CommandDefinition = {
     name: 'marshaller',
     description: 'The marshaller is always right',
     category: CommandCategory.MEMES,
     executor: (msg) => {
-        const addonEmbed = makeEmbed({ image: { url: ADDON_URL } });
-        return msg.channel.send({ embeds: [addonEmbed] });
+        const MARSHALLER_embed = makeEmbed({ image: { url: MARSHALLER_URL } });
+        return msg.channel.send({ embeds: [MARSHALLER_embed] });
     },
 };
