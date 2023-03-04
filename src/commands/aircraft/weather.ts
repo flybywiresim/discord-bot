1d2f421f654776324f5a8d04138b86452b253e7e
Michael Corcoran <tracer@outlook.co.nz>
2023-03-05 02:30:56 +1300

    fix: Improve wxr and terr command (#424)
    Co-authored-by: Ben Williams <bwilliams12@me.com>

--- src/commands/aircraft/weather.ts
+++ src/commands/aircraft/weather.ts
@@ -3,16 +3,16 @@ import { CommandCategory } from '../../constants';
 import { makeEmbed, makeLines } from '../../lib/embed';
 
 export const weather: CommandDefinition = {
-    name: ['weather', 'wx'],
+    name: ['weather', 'wx', 'wxr', 'terrain', 'terr', 'terronnd'],
     description: 'Explains the current state of the weather and terrain radars in experimental',
     category: CommandCategory.AIRCRAFT,
     executor: (msg) => {
         const weatherEmbed = makeEmbed({
             title: 'FlyByWire A32NX | Weather Radar + Terrain Display',
             description: makeLines([
-                'The A32NX does not have operating weather radar. This is due to performance issues related to the default Asobo systems and implementing a custom ND in our aircraft.',
+                'The A32NX does not have an operating weather radar as it is not currently possible to implement a high-fidelity A320 weather radar or weather map in MSFS.',
                 '',
-                'Terrain display is currently only available on the development + experimental version via [SimBridge.](https://docs.flybywiresim.com/simbridge/)',
+                'The terrain map (TERR on ND) is currently only available on the development + experimental versions via [SimBridge.](https://docs.flybywiresim.com/simbridge/)',
                 '',
                 'The team are currently waiting on a weather API to be implemented in order to make a radar that is as realistic as possible. You can read the MSFS forum [here.](https://forums.flightsimulator.com/t/implement-weather-and-terrain-api-s-for-aircraft-developers-to-implement-accurate-radar-predictive-windshear-egpws-and-metar-wind-uplink/442016)',
             ]),
