3408df3c2df670480451a99fe35a6f46dd615f88
Ben Williams <bwilliams12@me.com>
2023-03-06 14:46:35 +0000

    General

--- src/commands/general/roadmap.ts
+++ src/commands/general/roadmap.ts
@@ -1,16 +1,14 @@
-import { CommandDefinition } from '../../lib/command';
+import { MessageCommandDefinition } from '../../lib/command';
 import { makeEmbed } from '../../lib/embed';
 import { CommandCategory } from '../../constants';
 
 const ROADMAP_URL = 'https://cdn.discordapp.com/attachments/827579133964845107/909827187354533978/ROADMAP.png';
 
-export const roadmap: CommandDefinition = {
+const roadmapEmbed = makeEmbed({ image: { url: ROADMAP_URL } });
+
+export const roadmap: MessageCommandDefinition = {
     name: ['roadmap', 'goals'],
     category: CommandCategory.GENERAL,
     description: 'FBW Roadmap',
-    executor: (msg) => {
-        const roadmapEmbed = makeEmbed({ image: { url: ROADMAP_URL } });
-
-        return msg.channel.send({ embeds: [roadmapEmbed] });
-    },
+    genericEmbed: roadmapEmbed,
 };
--- src/commands/general/willithave.ts
+++ src/commands/general/willithave.ts
@@ -1,21 +1,19 @@
-import { CommandDefinition } from '../../lib/command';
+import { MessageCommandDefinition } from '../../lib/command';
 import { CommandCategory } from '../../constants';
 import { makeEmbed, makeLines } from '../../lib/embed';
 
-export const willithave: CommandDefinition = {
+const willithaveEmbed = makeEmbed({
+    title: 'Will the aircraft have [FEATURE]?',
+    description: makeLines([
+        'The FBW rule of thumb is:',
+        '',
+        'If it\'s in the real aircraft, it\'ll be there.',
+    ]),
+});
+
+export const willithave: MessageCommandDefinition = {
     name: ['thumb', 'willithave'],
     description: 'Answers the big question, will it have FEATURE?',
     category: CommandCategory.GENERAL,
-    executor: (msg) => {
-        const willithaveEmbed = makeEmbed({
-            title: 'Will the aircraft have [FEATURE]?',
-            description: makeLines([
-                'The FBW rule of thumb is:',
-                '',
-                'If it\'s in the real aircraft, it\'ll be there.',
-            ]),
-        });
-
-        return msg.channel.send({ embeds: [willithaveEmbed] });
-    },
+    genericEmbed: willithaveEmbed,
 };
