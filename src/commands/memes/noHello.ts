c015fbccdcbf8b0fae7a0c8e467b932449722fee
Sleinmaster <79196358+Sleinmaster@users.noreply.github.com>
2023-03-20 10:07:48 +0100

    fix linting error and forgotten await

--- src/commands/memes/noHello.ts
+++ src/commands/memes/noHello.ts
@@ -1,6 +1,6 @@
+import { Message } from 'discord.js';
 import { CommandDefinition } from '../../lib/command';
 import { CommandCategory } from '../../constants';
-import { Message } from 'discord.js';
 
 const NO_HELLO_URL = 'https://nohello.net/';
 
@@ -9,9 +9,10 @@ export const noHello: CommandDefinition = {
     description: 'No!',
     category: CommandCategory.MEMES,
     executor: (msg: Message) => {
-        const sentMsg = msg.fetchReference()
-        .then((res) => res.reply(NO_HELLO_URL))
-        .catch(() => msg.channel.send(NO_HELLO_URL));
+        const sentMsg = await msg
+          .fetchReference()
+          .then((res) => res.reply(NO_HELLO_URL))
+          .catch(() => msg.channel.send(NO_HELLO_URL));
 
         msg.delete();
         return sentMsg;
