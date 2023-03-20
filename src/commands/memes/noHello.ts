bfb6ce1146052cc04021295d92c865832751cb7b
Sleinmaster <79196358+Sleinmaster@users.noreply.github.com>
2023-03-20 10:03:18 +0100

    linting correction

--- src/commands/memes/noHello.ts
+++ src/commands/memes/noHello.ts
@@ -1,5 +1,6 @@
-import { CommandDefinition, replyWithMsg } from '../../lib/command';
+import { CommandDefinition } from '../../lib/command';
 import { CommandCategory } from '../../constants';
+import { Message } from 'discord.js';
 
 const NO_HELLO_URL = 'https://nohello.net/';
 
@@ -7,11 +8,11 @@ export const noHello: CommandDefinition = {
     name: ['nohello', 'hello'],
     description: 'No!',
     category: CommandCategory.MEMES,
-    executor: (msg) => {
+    executor: (msg: Message) => {
         const sentMsg = msg.fetchReference()
         .then((res) => res.reply(NO_HELLO_URL))
         .catch(() => msg.channel.send(NO_HELLO_URL));
-        
+
         msg.delete();
         return sentMsg;
     },
