50fbc6f77c2afdf687fd7605aa6c2fc4e69eb33c
sleinmaster <niels@slein.ch>
2023-03-21 21:39:42 +0100

    feature: command for tca key binding problems

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -163,6 +163,7 @@ import { slowMode } from './moderation/slowmode';
 import { econnreset } from './support/econnreset';
 import { pr } from './utils/pr';
 import { gsxIntegration } from './aircraft/gsxIntegration';
+import { tca } from './support/tca';
 
 const commands: BaseCommandDefinition[] = [
     typeCommand,
@@ -328,6 +329,7 @@ const commands: BaseCommandDefinition[] = [
     econnreset,
     pr,
     gsxIntegration,
+    tca,
 ];
 
 const commandsObject: { [k: string]: BaseCommandDefinition } = {};
