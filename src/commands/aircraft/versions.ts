f49e1cc8d291b21c755302fd4b3a43c636b6ce71
benw202 <bwilliams12@me.com>
2023-05-15 19:52:29 +0100

    update versions links

--- src/commands/aircraft/versions.ts
+++ src/commands/aircraft/versions.ts
@@ -16,7 +16,7 @@ const versionsEmbed = makeEmbed({
             value: makeLines([
                 '> Stable is our version which has features that are the most mature and most tested. This should be a reliable version for those preferring stability over newest features. Will be compatible with each major MSFS patch.',
                 '',
-                '> Use the installer or [download here](https://api.flybywiresim.com/api/v1/download?url=https://flybywiresim-packages.b-cdn.net/stable/A32NX-stable.zip)',
+                '> Use the installer or [download here](https://github.com/flybywiresim/a32nx/releases/download/assets/stable/A32NX-stable.zip)',
             ]),
             inline: false,
         },
@@ -25,7 +25,7 @@ const versionsEmbed = makeEmbed({
             value: makeLines([
                 '> Development will have the latest features that will eventually end up in the next stable release. In general this version has the latest fixes and newest features but also a slightly higher risk of containing bugs. Development updates whenever a change is made to the "master" branch on Github.',
                 '',
-                '> Use the installer or [download here](https://api.flybywiresim.com/api/v1/download?url=https://flybywiresim-packages.b-cdn.net/vmaster/A32NX-master.zip)',
+                '> Use the installer or [download here](https://github.com/flybywiresim/a32nx/releases/download/assets/master/A32NX-master.zip)',
             ]),
             inline: false,
         },
