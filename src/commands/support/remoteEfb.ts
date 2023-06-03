4e98f35047e43952893b9820af5a7cb6bd16b3ef
Lauvaan <122087991+Lauvaan@users.noreply.github.com>
2023-06-03 22:31:30 +0300

    Update src/commands/support/remoteEfb.ts
    Co-authored-by: Philippe Dellaert <philippe@dellaert.org>

--- src/commands/support/remoteEfb.ts
+++ src/commands/support/remoteEfb.ts
@@ -7,7 +7,7 @@ const remoteEfbEmbed = makeEmbed({
     description: makeLines([
         'Currently the EFB cannot be viewed remotely, but it is a planned feature.',
         '',
-        'In the meantime, you can however use an application like [Spacedesk](https://www.spacedesk.net/)* to create a virtual screen using yout tablet/phone/pc, and then de-attach the EFB from MSFS by holding the ALTGR button and clicking on the EFB with your mouse. You can then drag the EFB window to your virtual screen. Please note that this solution will degrade your performance slightly, as your computer needs to render and process the new virtual screen.',
+        'In the meantime, you can however use an application like [Spacedesk](https://www.spacedesk.net/)* to create a virtual screen using your tablet/phone/pc, and then de-attach the EFB from MSFS by holding the ALTGR button and clicking on the EFB with your mouse. You can then drag the EFB window to your virtual screen. Please note that this solution will degrade your performance slightly, as your computer needs to render and process the new virtual screen.',
     ]),
     footer: { text: '*Spacedesk is not associated with FlyByWire Simulations in any way. We only suggest this solution as it has been proven to work in the past.' },
 });
