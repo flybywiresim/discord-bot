6df4749579468d2d10ff847caddab5f1fc49dd35
Lauvaan <thelauvaan.vaananen@gmail.com>
2023-06-13 10:46:56 +0300

    FIx PR/lint(pull_request) problems in remoteefb.ts

--- src/commands/support/remoteEfb.ts
+++ src/commands/support/remoteEfb.ts
@@ -7,9 +7,9 @@ const remoteEfbEmbed = makeEmbed({
     description: makeLines([
         'Currently the EFB cannot be viewed remotely, but it is a planned feature.',
         '',
-        'In the meantime, you can however use an application like [spacedesk](https://www.spacedesk.net/)* to create a virtual screen using your tablet/phone/pc, and then detach the EFB from MSFS by holding the \`ALTGR`\ button and clicking on the EFB with your mouse. You can then drag the EFB window to your virtual screen.',
+        'In the meantime, you can however use an application like [spacedesk](https://www.spacedesk.net/)* to create a virtual screen using your tablet/phone/pc, and then detach the EFB from MSFS by holding the `ALTGR` button and clicking on the EFB with your mouse. You can then drag the EFB window to your virtual screen.',
         '',
-        'If you want to use touch inputs with the efb on your virtual screen, you need to use a program like [MSFS Pop Out Panel Manager](https://flightsim.to/file/35759/msfs-pop-out-panel-manager) as MSFS doesn\'t\ directly support this via spacedesk.', 
+        'If you want to use touch inputs with the efb on your virtual screen, you need to use a program like [MSFS Pop Out Panel Manager](https://flightsim.to/file/35759/msfs-pop-out-panel-manager) as MSFS doesn\'t\ directly support this via spacedesk.',
         '',
         'Please note that this solution will degrade your performance slightly, as your computer needs to render and process the new virtual screen.',
 
