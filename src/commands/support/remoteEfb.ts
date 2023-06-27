3b9eac0ebf7dcd466c3cb481fbe76e46615ab526
Lauvaan <122087991+Lauvaan@users.noreply.github.com>
2023-06-28 01:19:48 +0300

    fix: Add missing information about touch inputs when using the unofficial remote efb workaround, minor text formatting (#471)
    * Add info about touch inputs remtoeEfb.ts
    
    * FIx PR/lint(pull_request) problems in remoteefb.ts
    
    * Add changelog.md entries
    
    * Fix more PR/lint problems in remoteEfb.ts
    
    * fix grammar/punctuation errors
    
    * fix capitalisation
    
    ---------
    
    Co-authored-by: NUT <87286435+NUTTTTTTTT@users.noreply.github.com>

--- src/commands/support/remoteEfb.ts
+++ src/commands/support/remoteEfb.ts
@@ -7,9 +7,14 @@ const remoteEfbEmbed = makeEmbed({
     description: makeLines([
         'Currently the EFB cannot be viewed remotely, but it is a planned feature.',
         '',
-        'In the meantime, you can however use an application like [spacedesk](https://www.spacedesk.net/)* to create a virtual screen using your tablet/phone/pc, and then de-attach the EFB from MSFS by holding the ALTGR button and clicking on the EFB with your mouse. You can then drag the EFB window to your virtual screen. Please note that this solution will degrade your performance slightly, as your computer needs to render and process the new virtual screen.',
+        'In the meantime, you can, however, use an application such as [spacedesk](https://www.spacedesk.net/)* to create a virtual screen using your tablet/phone/PC, and detach the EFB from MSFS by holding the `ALTGR` button and clicking on the EFB with your mouse. You can then drag the EFB window to your virtual screen.',
+        '',
+        'If you want to use touch inputs with the efb on your virtual screen, you need to use a program like [MSFS Pop Out Panel Manager](https://flightsim.to/file/35759/msfs-pop-out-panel-manager) as MSFS doesn\'t directly support this via spacedesk.',
+        '',
+        'Please note that this solution will degrade your performance slightly, as your computer needs to render and process the new virtual screen.',
+
     ]),
-    footer: { text: '*Spacedesk is not associated with FlyByWire Simulations in any way. We only suggest this solution as it has been proven to work in the past.' },
+    footer: { text: '*Neither spacedesk nor MSFS Popout Panel Manager are associated with FlyByWire Simulations in any way. We only suggest this solution as they have been proven to work in the past.' },
 });
 
 export const remoteEfb: MessageCommandDefinition = {
