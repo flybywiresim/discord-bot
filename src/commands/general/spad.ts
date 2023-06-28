2031e607cc87aa7e0a0f43ce399b71a151812648
Philippe Dellaert <philippe@dellaert.org>
2023-06-28 14:09:02 -0700

    fix: Fix URL based on Spad.neXt website (#477)

--- src/commands/general/spad.ts
+++ src/commands/general/spad.ts
@@ -5,5 +5,5 @@ export const spad: CommandDefinition = {
     name: ['spad'],
     description: 'Provides link to the SPAD.neXt discord server',
     category: CommandCategory.GENERAL,
-    executor: (msg) => replyWithMsg(msg, 'https://discord.gg/eT94dm5peg'),
+    executor: (msg) => replyWithMsg(msg, 'https://discord.gg/sRwcWkB'),
 };
