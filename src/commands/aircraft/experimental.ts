eb87b9b8e812f0275d62039c50c07923a0faa218
Sleinmaster <79196358+Sleinmaster@users.noreply.github.com>
2023-06-28 07:50:41 +0200

    add experimental not being updated.

--- src/commands/aircraft/experimental.ts
+++ src/commands/aircraft/experimental.ts
@@ -5,9 +5,9 @@ import { makeEmbed, makeLines } from '../../lib/embed';
 const experimentalEmbed = makeEmbed({
     title: 'FlyByWire A32NX | Experimental Version',
     description: makeLines([
-        'Our Experimental version is temporarily on hold and all of its features have been moved to the Development Version. ',
+        'Our Experimental version is temporarily on hold and is not being updated. All of its features have been moved to the Development Version. ',
         '',
-        'The function of the Experimental version is to test high impact features which aren't ready for the wider public. As there are currently no such features being tested and it being easier to introduce new features into the Development version and maintain it, it was decided to put the Experimental version hold.',
+        'The function of the Experimental version is to test high-impact features which aren't ready for the wider public yet. As there are currently no such features being tested and it being easier to introduce new features into the Development version and maintain it, it was decided to put the Experimental version on hold.',
     ]),
 });
 
