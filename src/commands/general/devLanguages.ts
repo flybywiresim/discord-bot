cc9500ac16e2bee9bd936d4865e945e23f3e79c7
NUT <87286435+NUTTTTTTTT@users.noreply.github.com>
2023-07-02 16:33:01 +0100

    Update src/commands/general/devLanguages.ts
    Co-authored-by: BenW <bwilliams12@me.com>

--- src/commands/general/devLanguages.ts
+++ src/commands/general/devLanguages.ts
@@ -5,7 +5,7 @@ import { makeEmbed, makeLines } from '../../lib/embed';
 const devLanguagesEmbed = makeEmbed({
     title: 'FlyByWire A32NX | Development Languages',
     description: makeLines([
-        'The A32NX is built using a number of development languages for different aspects of the aircraft. The below list confirms the current languages used, however is not exhaustive.',
+        'The A32NX is built using a number of development languages for different aspects of the aircraft. The list below confirms the current languages used, however is not exhaustive.',
         '',
         '**General:** React',
         '**flyPad:** Tailwind CSS, Redux',
