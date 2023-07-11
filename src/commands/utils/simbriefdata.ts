167882fcd7d128852e264948cbb2bf7ab643a8c4
sleinmaster <niels@slein.ch>
2023-07-11 21:34:38 +0200

    feat: simbriefdata optimize for input error

--- src/commands/utils/simbriefdata.ts
+++ src/commands/utils/simbriefdata.ts
@@ -5,11 +5,13 @@ import { CommandCategory } from '../../constants';
 import { CommandDefinition, replyWithEmbed } from '../../lib/command';
 import { makeEmbed, makeLines } from '../../lib/embed';
 
+const FBW_AIRFRAME_ID = '337364_1631550522735';
+
 const simbriefEmded = (flightplan) => makeEmbed({
     title: 'SimBrief Data',
     description: makeLines([
         `**Generated at**: ${moment(flightplan.params.time_generated * 1000).format('DD.MM.YYYY, HH:mm:ss')}`,
-        `**AirFrame**: ${flightplan.aircraft.name} ${flightplan.aircraft.internal_id}`,
+        `**AirFrame**: ${flightplan.aircraft.name} ${flightplan.aircraft.internal_id} ${(flightplan.aircraft.internal_id === FBW_AIRFRAME_ID) ? '(provided by FBW)' : ''}`,
         `**AIRAC Cycle**: ${flightplan.params.airac}`,
         `**Origin**: ${flightplan.origin.icao_code}`,
         `**Destination**: ${flightplan.destination.icao_code}`,
@@ -21,7 +23,7 @@ const simbriefEmded = (flightplan) => makeEmbed({
 const simbriefIdMismatchEmbed = (enteredId, flightplanId) => makeEmbed({
     title: 'SimBrief Data',
     description: makeLines([
-        `Entered userId ${enteredId} and returned userId ${flightplanId} don't match. The userId might be used as username by someone else.`,
+        `Entered pilotId ${enteredId} and returned pilotId ${flightplanId} don't match. The pilotId might be used as username by someone else.`,
     ]),
 });
 
@@ -35,10 +37,17 @@ const simbriefdatarequestEmbed = makeEmbed({
     title: 'FlyByWire Support | SimBrief Data Request',
     description: makeLines([
         'To evaluate your problem we kindly ask you to enter the following bot command into a new message.',
-        '```.simbriefdata <userId>```',
-        'Replace <userId> incl. the brackets with your simbrief userId or userName. The Bot will read your last generated flight plan and display some details about it incl. the route.',
+        '```.simbriefdata <pilotId>```',
+        'Replace <pilotId> incl. the brackets with your simbrief pilotId or userName (as set in the EFB settings). The Bot will read your last generated flight plan and display some details about it incl. the route.',
         '',
-        '**Privacy notice**: If you share your userId or username it is possible to read your pilot name from the API the bot uses. This pilot name is by default your real name, but you can change it in the flight edit screen or your user profile in SimBrief. No data is stored by FlyByWire when using the command.',
+        '**Privacy notice**: If you share your pilotId or username it is possible to read your pilot name from the API the bot uses. This pilot name is by default your real name, but you can change it in the flight edit screen or your user profile in SimBrief. No data is stored by FlyByWire when using the command.',
+    ]),
+});
+
+const pilotIdNotReplacedEmbed = makeEmbed({
+    title: 'FlyByWire Support | SimBrief Data Input Error',
+    description: makeLines([
+        'Please replace the *<pilotId>* in the used command with your simbrief pilotId (as set in the EFB settings).',
     ]),
 });
 
@@ -48,9 +57,17 @@ export const simbriefdata: CommandDefinition = {
     category: CommandCategory.UTILS,
     executor: async (msg) => {
         const splitUp = msg.content.split(' ').slice(1);
-        const simbriefId = splitUp[0];
+        let simbriefId = splitUp[0];
         if (simbriefId === undefined || simbriefId === null) {
             return replyWithEmbed(msg, simbriefdatarequestEmbed);
+        }
+
+        if (simbriefId === '<pilotId>') {
+            return replyWithEmbed(msg, pilotIdNotReplacedEmbed);
+        }
+
+        if (simbriefId.startsWith('<') && simbriefId.endsWith('>')) {
+            simbriefId = simbriefId.slice(1).slice(0, -1);
         }
 
         const flightplan = await fetch(`https://www.simbrief.com/api/xml.fetcher.php?json=1&userid=${simbriefId}&username=${simbriefId}`).then((res) => res.json());
