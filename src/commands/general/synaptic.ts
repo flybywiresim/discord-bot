7770bd8f58cde08722b317b8681ffba77997c426
Philippe Dellaert <philippe@dellaert.org>
2024-01-30 21:14:03 -0800

    fix: Removing Synaptic command

--- src/commands/general/synaptic.ts
+++
@@ -1,9 +0,0 @@
-import { CommandDefinition, replyWithMsg } from '../../lib/command';
-import { CommandCategory } from '../../constants';
-
-export const synaptic: CommandDefinition = {
-    name: ['synaptic', 'syn'],
-    description: 'Provides link to synaptic discord server',
-    category: CommandCategory.GENERAL,
-    executor: (msg) => replyWithMsg(msg, 'https://discord.gg/acQkSvrePG'),
-};
