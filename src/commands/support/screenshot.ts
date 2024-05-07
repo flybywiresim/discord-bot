ff85d9341b00610981797bf20e5091aa3f7bb106
ExampleWasTaken <58574351+ExampleWasTaken@users.noreply.github.com>
2024-05-07 23:49:09 +0200

    improve screenshot cmd

--- src/commands/support/screenshot.ts
+++ src/commands/support/screenshot.ts
@@ -5,10 +5,13 @@ import { CommandCategory, imageBaseUrl } from '../../constants';
 const SCREENSHOT_HELP_URL = `${imageBaseUrl}/support/screenshot.jpg`;
 
 const screenshotEmbed = makeEmbed({
-    title: 'FlyByWire Support | How to take a good screenshot',
+    title: 'FlyByWire Support | Aircraft State',
     description: makeLines([
-        'Position yourself in the cockpit using the arrow keys to look straight at the front instrument panel. Then use the Windows Snipping Tool to take a clear screenshot of all screens and the FCU as shown.',
-        'Please read the guide [here](https://docs.flybywiresim.com/fbw-a32nx/support/#screenshot-of-cockpit) for more information.',
+        'In order to provide efficient support, we need to see the state of your aircraft and its systems.',
+        '',
+        'Please position yourself in the cockpit using the arrow keys to look straight at the front instrument panel. Then use the [Windows Snipping Tool](https://support.microsoft.com/en-us/windows/open-snipping-tool-and-take-a-screenshot-a35ac9ff-4a58-24c9-3253-f12bac9f9d44) to take a clear screenshot of **all screens and the entire FCU** as shown.',
+        '',
+        'Read the guide [here](https://docs.flybywiresim.com/fbw-a32nx/support/#screenshot-of-cockpit) for more information.',
     ]),
     image: { url: SCREENSHOT_HELP_URL },
 });
