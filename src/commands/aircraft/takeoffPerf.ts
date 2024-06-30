ccf44cf4b28b97cb7c9b71f54dcd12627f9367b7
Philippe Dellaert <philippe@dellaert.org>
2024-06-30 11:37:33 -0700

    Updating commands to reflect release of calculator for Developer

--- src/commands/aircraft/takeoffPerf.ts
+++ src/commands/aircraft/takeoffPerf.ts
@@ -1,10 +1,16 @@
 import { MessageCommandDefinition } from '../../lib/command';
 import { CommandCategory } from '../../constants';
-import { makeEmbed } from '../../lib/embed';
+import { makeEmbed, makeLines } from '../../lib/embed';
 
 const takeoffPerfEmbed = makeEmbed({
-    title: 'FlyByWire A32NX | Where is the takeoff calculator?',
-    description: 'A takeoff calculator for the A32NX is currently under development.',
+    title: 'FlyByWire A32NX | Takeoff Performance Calculator',
+    description: makeLines([
+        'A Takeoff Performance Calculator is available on the **Developer** version of the A32NX. For more information, check out the [Takeoff Performance Calculator documentation](https://docs.flybywiresim.com/flypad-performance/).',
+        '',
+        'For the **Stable** version, see the [V-Speeds Section](https://docs.flybywiresim.com/pilots-corner/beginner-guide/preparing-mcdu/#v-speeds) in our MCDU guide.',
+        '',
+        '**Note:** The functionality where the LSK next to the V1, VR and V2 speeds can be used to automatically calculate them in the MCDU PERF page has been removed in the Developer version. Use the new Takeoff Performance Calculator instead.',
+    ]),
 });
 
 export const takeoffPerf: MessageCommandDefinition = {
